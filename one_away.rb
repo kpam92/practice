277b8374c903e1cce36cc4350ff8a05774fa3828
kpam92 <kpam1992@gmail.com>
2016-11-06 16:54:44 -0800

    one_way problem changes

--- one_away.rb
+++ one_away.rb
@@ -1,2 +1,25 @@
 # Given two strings, write a function
  # to check if they are one edit(or zero edits) away.
+
+def one_away(string1, string2)
+
+  return true if string1 == string2
+  one_change = 0
+  strings = [string1, string2].sort {|x,y| x.length <=> y.length}
+  string1 = strings[1]
+  string2 = strings[0]
+  return false if (string1.length - string2.length) > 1
+  idx1 = 0
+  idx2 = 0
+
+  while idx1 < string1.length
+
+    if string1[idx1] != string2[idx2]
+      one_change += 1
+      idx2 += 1 if string1.length == string2.length
+    end
+    idx1 += 1
+    return false if one_change > 1
+  end
+  true
+end

46ae9a5f2b9f19d004057273df7b38742aad6714
kpam92 <kpam1992@gmail.com>
2016-11-06 17:30:51 -0800

    finished problem

--- check_permutation.rb
+++ check_permutation.rb
@@ -1,4 +1,20 @@
 # Given two strings, write a method to decide if one is a permutation of the other
 
 def check_permutation?(string1,string2)
+
+  return true if string1 == string2
+  return false if string1.length != string2.length
+  result = Hash.new(0)
+
+  string1.chars.each do |letter|
+    result[letter] += 1
+  end
+
+  string2.chars.each do |letter|
+    result[letter] -= 1
+  end
+
+  result.each {|h,k| return false if k != 0}
+
+  true
 end

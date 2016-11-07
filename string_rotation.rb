f4017dee22c9962b2d7bd0a09818a9e7a5162fc1
kpam92 <kpam1992@gmail.com>
2016-11-06 19:54:08 -0800

    initiated problem

---
+++ string_rotation.rb
@@ -0,0 +1,8 @@
+#write a method that checks to see if a string is a rotated version of another
+# and uses only one call to a helper method is_substring once
+
+def is_substring?(string1, string2)
+end
+
+def string_rotation?(string1,string2)
+end

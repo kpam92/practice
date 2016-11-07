09bf013f999462625e0b6e9c78c2df7523842675
kpam92 <kpam1992@gmail.com>
2016-11-06 17:46:08 -0800

    finished problem with an extra data structure

--- is_unique.rb
+++ is_unique.rb
@@ -1,4 +1,16 @@
 # Implement an algorithm to check if a string has all unique characters
+# Also try to implement without another data structure
 
-def is_unique?(string)
+def is_unique_one?(string)
+  
+end
+
+def is_unique_two?(string)
+  seen = Hash.new
+
+  string.chars.each do |letter|
+    return false if seen[letter] == true
+    seen[letter] = true
+  end
+  true
 end

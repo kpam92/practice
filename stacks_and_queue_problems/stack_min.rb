59444b12dd26260ad396152603266c0f669e5c50
kpam92 <kpam1992@gmail.com>
2016-11-07 21:49:42 -0800

    working through problem

--- stacks_and_queue_problems/stack_min.rb
+++ stacks_and_queue_problems/stack_min.rb
@@ -3,8 +3,20 @@
 
 class Stack
 
-attr_accessor :min
+  attr_accessor :min
 
-def initialize()
-  @min = nil
+  def initialize()
+    @min = nil
+    @values = []
+  end
+
+  def push(val)
+    @values.push(val)
+    @min = val if (val < @min) || @min.nil?
+  end
+
+  def pop
+    
+  end
+
 end

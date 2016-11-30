768a592f4584cd05668f1e477725833e596c336c
kpam92 <kpam1992@gmail.com>
2016-11-30 14:18:19 -0800

    started new problem base for kth to last item in a linked list

---
+++ kth_to_last_node.rb
@@ -0,0 +1,13 @@
+class LinkedListNode
+
+  attr_accessor :value, :next
+
+  def initialize(value)
+      @value = value
+      @next  = nil
+  end
+end
+
+def kth_to_last_node(int, head_node)
+  
+end

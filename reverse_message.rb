7a14d383bd1bf1fb341b343530698cf048fd5bca
kpam92 <kpam1992@gmail.com>
2016-11-30 15:52:46 -0800

    finished initial problem

--- reverse_message.rb
+++ reverse_message.rb
@@ -1,4 +1,14 @@
 #reverse words in message, assuming all words are seperated by only spaces
 
-def reverse_message
+def reverse_message(string)
+  string = string.split(" ")
+  idx1 = 0
+
+  while idx1 < string.length/2
+    idx2 = string.length - (idx1 + 1)
+
+    string[idx1], string[idx2] = string[idx2], string[idx1]
+
+  end
+  string.join(" ")
 end

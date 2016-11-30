453f7c2061f3ed06e8f4e01c5f446f257b43858e
kpam92 <kpam1992@gmail.com>
2016-11-30 15:32:38 -0800

    working on prob

--- reverse_string.rb
+++ reverse_string.rb
@@ -1,5 +1,12 @@
 #reverse a string in place
 
 def reverse_string(string)
-  
+  idx1 = 0
+  while idx1 < string.length/2
+    idx2 = (string.length - 1) - idx1
+    string[idx1], string[idx2] = string[idx2], string[idx1]
+    idx1 += 1
+  end
+
+  string
 end

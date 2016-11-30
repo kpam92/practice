62bf1c41c2622034be5851fcde19ba78863669ae
kpam92 <kpam1992@gmail.com>
2016-11-30 14:10:58 -0800

    finished explanations to problem

--- reversed_linked_list.rb
+++ reversed_linked_list.rb
@@ -1,3 +1,5 @@
+
+#this linked list class only keeps track of the next node in line, only going one way
 class LinkedListNode
 
   attr_accessor :value, :next
@@ -8,7 +10,8 @@ class LinkedListNode
   end
 end
 
-
+#given only the head of a linked list, create a method that reverses the list
+# and takes 0(1) space, and 0(N) time
 def reverse_linked_list(head_node)
   prev_node = nil
 

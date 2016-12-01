c925b5ab7dc34ccc113b484eae6f6411ec7260c3
kpam92 <kpam1992@gmail.com>
2016-11-30 16:31:20 -0800

    working through solution

--- parenthetical.rb
+++ parenthetical.rb
@@ -22,3 +22,24 @@ closing = []
 closing[opening.length - (1 + opening.index(position))]
 
 end
+
+
+
+def par(string, position)
+  open_count = 0
+
+  string.chars.each_with_index do |el,idx|
+
+    if el == "("
+      open_count += 1
+      target_count = open_count if idx == position
+    elsif el == ")"
+      open_count -= 1
+      return idx if open_count == target_count
+    end
+
+  end
+
+closing[opening.length - (1 + opening.index(position))]
+
+end

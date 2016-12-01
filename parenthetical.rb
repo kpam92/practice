8c6fdf75ad2fd7427e7f86816fb82089331e43da
kpam92 <kpam1992@gmail.com>
2016-11-30 16:23:22 -0800

    finished initial problem

--- parenthetical.rb
+++ parenthetical.rb
@@ -4,5 +4,21 @@
 # Write a function that, given a sentence like the one above,
 #  along with the position of an opening parenthesis,
 #   finds the corresponding closing parenthesis.
-def par(position)
+
+def par(string, position)
+opening = []
+closing = []
+
+  string.chars.each_with_index do |el,idx|
+
+    if el == "("
+      opening << idx
+    elsif el == ")"
+      closing << idx
+    end
+
+  end
+
+closing[opening.length - (1 + opening.index(position))]
+
 end

5bdf1eeedc93ea4e2a7d02d3c5afc594977396ce
kpam92 <kpam1992@gmail.com>
2016-11-30 16:09:40 -0800

    created parenthetical problem

---
+++ parenthetical.rb
@@ -0,0 +1,8 @@
+# "Sometimes (when I nest them (my parentheticals)
+#  too much (like this (and this))) they get confusing."
+#
+# Write a function that, given a sentence like the one above,
+#  along with the position of an opening parenthesis,
+#   finds the corresponding closing parenthesis.
+def par(position)
+end

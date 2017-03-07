8ed8b2b6eb5411ba360ec0155ed547cc391286d8
kpam92 <kpam1992@gmail.com>
2017-03-06 18:48:58 -0800

    finish helper method

--- amne_challenge.rb
+++ amne_challenge.rb
@@ -1,5 +1,17 @@
 
 
-def sub_array
-  
+def sub_array(array)
+  idx1 = 0
+  idx2 = 1
+  curr_count = array[idx1]
+  while idx2 < array.length
+    curr_count += array[idx2]
+    if array[idx2] == array[idx1]
+      curr_count += (array[idx2] * (idx2 - idx1))
+    else
+      idx1 = idx2
+    end
+    idx2 += 1
+  end
+  curr_count
 end

bc9d06a2937b899a3ee8ecca0a51916843ea071a
kpam92 <kpam1992@gmail.com>
2017-04-04 14:50:51 -0700

    cool numbers

--- ethos.rb
+++ ethos.rb
@@ -1,2 +1,19 @@
+require 'set'
+require 'byebug'
+
 def cool_number?(num)
+  new_number = num
+  seen_numbers = Set.new
+
+  cool = false
+
+  until cool
+    # byebug
+    new_number = new_number.to_s.chars.map {|x| x.to_i**2}.reduce(:+)
+    return false if (seen_numbers.include?(new_number) || new_number == 4)
+    seen_numbers.add(new_number)
+    return true if new_number == 1
+  end
 end
+
+cool_number?(23)

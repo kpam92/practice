5c6a971cdc11e044a54da6e585cecf7001b6281e
kpam92 <kpam1992@gmail.com>
2017-04-04 16:22:55 -0700

    prototypal inheritance

---
+++ prototype_inheritance.js
@@ -0,0 +1,14 @@
+
+
+
+class Animal {
+  constructor(name,age){this.name = name;this.age = age};
+  eat() {return `nom nom nom`;}
+}
+
+
+
+class Dog {
+  constructor(name,age){this.name = name;this.age = age};
+  woof() {return `${this.name} goes woof`;}
+}

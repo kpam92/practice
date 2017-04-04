afe95896f4908f614e0911dffc8ce6ef96be6258
kpam92 <kpam1992@gmail.com>
2017-04-04 12:14:14 -0700

    bubble sort

--- sorting_refresher.rb
+++ sorting_refresher.rb
@@ -1,15 +1,18 @@
 # a quick dive back into the various different manners of sorting.
 
 def bubble_sort(array)
-
   sorted = false
+
+  # this loop will run until the entire array is sorted
   until sorted
     sorted = true
     idx = 0
-    while idx < array.length
+    while idx < array.length - 1
       if array[idx] > array[idx + 1]
+        # if unsorted numbers found, array is marked unsorted
+        # for this pass, and numbers are switched
         sorted = false
-        array[idx],array[idx + 1] = array[idx + 1], array[idx1]
+        array[idx], array[idx + 1] = array[idx + 1], array[idx]
       end
       idx += 1
     end

f7d10de935d1cce1bc0d313f17a2507f88436d62
kpam92 <kpam1992@gmail.com>
2017-04-04 16:56:18 -0700

    prototypal inheritance

--- prototype_inheritance.js
+++ prototype_inheritance.js
@@ -1,14 +1,66 @@
 
 
+// We want to make all methods of animal accessible to any instance of dog
 
-class Animal {
-  constructor(name,age){this.name = name;this.age = age};
-  eat() {return `nom nom nom`;}
+
+// old version of creating prototypal inheritance of dog to animal
+function Animal (name) {
+  this.name = name;
+};
+
+Animal.prototype.eat = function () {
+  console.log("nom nom nom");
+};
+
+function Dog (name, age) {
+  Animal.call(this, name);
+
+  this.age = age;
+}
+
+Dog.prototype.woof = function() {
+  console.log(`${this.name} goes woof woof`)
 }
 
 
+function Surrogate() {};
+Surrogate.prototype = Animal.prototype;
+Dog.prototype = new Surrogate();
 
-class Dog {
-  constructor(name,age){this.name = name;this.age = age};
-  woof() {return `${this.name} goes woof`;}
+
+// ES2015 extending through class
+
+class Vehicle {
+  constructor(color, model) {
+    this.color = color;
+    this.model = model;
+  }
+
+  move() {
+    return "just movin' along here";
+  }
+}
+
+class Hummer extends Vehicle {
+  constructor(color, model, size) {
+    super(color, model);
+    this.size = size;
+  }
+
+  move() {
+    const oldMove = super.move();
+    return `${oldMove} VROOM VROOM`
+  }
+}
+
+class Prius extends Vehicle {
+  constructor(color, model, hybrid) {
+    super(color, model);
+    this.hybrid = hybrid;
+  }
+
+  move() {
+    const oldMove = super.move();
+    return `${oldMove} ~~silence~~`
+  }
 }

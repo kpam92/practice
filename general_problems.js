2ed418d9b21dd1f6c85c3a851542f80e26706d3a
kpam92 <kpam1992@gmail.com>
2017-04-04 21:09:42 -0700

    practice problems

---
+++ general_problems.js
@@ -0,0 +1,35 @@
+// write String.prototype.mySlice. It should take a start index and an
+// (optional) end index.
+
+String.prototype.mySlice = function() {
+  ctx = this;
+  start = arguments[0]
+  end = arguments[1] === undefined ? this.length : arguments[1];
+  end > this.length ? end = this.length : end = end;
+  result = ""
+  for(start; start < end; start++) {
+    result += ctx[start];
+  }
+  return result;
+}
+
+// write Array.prototype.myReduce (analogous to Ruby's Array#inject).
+
+// write Array.prototype.quickSort(comparator). Here's a quick refresher if
+// you've forgotten how quickSort works:
+//   - choose a pivot element from the array (usually the first)
+//   - for each remaining element of the array:
+//     - if the element is less than the pivot, put it in the left half of the
+//     array.
+//     - otherwise, put it in the right half of the array.
+//   - recursively call quickSort on the left and right halves, and return the
+//   full sorted array.
+
+// write myFind(array, callback). It should return the first element for which
+// callback returns true, or undefined if none is found.
+
+// write sumNPrimes(n)
+
+// write Function.prototype.myBind.
+
+// write Function.prototype.inherits.

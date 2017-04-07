e41ef090b17e02b038d00bd118cad4a8dd2d689c
kpam92 <kpam1992@gmail.com>
2017-04-06 20:55:42 -0700

    sql

--- sql/sqlzoo/lib/05_aggregates.rb
+++ sql/sqlzoo/lib/05_aggregates.rb
@@ -83,6 +83,14 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
+  SELECT
+  continent, COUNT(name)
+  FROM
+  countries
+  WHERE
+  population >= 10000000
+  GROUP BY
+  continent
   SQL
 end
 

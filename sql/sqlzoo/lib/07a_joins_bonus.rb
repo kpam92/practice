91ecd60c9e2fed39b1537306704a7a571c1f30a8
kpam92 <kpam1992@gmail.com>
2017-04-06 22:36:07 -0700

    sql

--- sql/sqlzoo/lib/07a_joins_bonus.rb
+++ sql/sqlzoo/lib/07a_joins_bonus.rb
@@ -70,6 +70,18 @@ def heart_tracks
   # the word 'Heart' (albums with no such tracks need not be shown). Order first by
   # the number of such tracks, then by album title.
   execute(<<-SQL)
+  SELECT
+  a.title, COUNT(*)
+  FROM
+  albums a
+  JOIN
+  tracks t ON t.album = a.asin
+  WHERE
+  t.song LIKE '%Heart%'
+  GROUP BY
+  a.title
+  ORDER BY
+  COUNT(*) DESC, a.title
   SQL
 end
 

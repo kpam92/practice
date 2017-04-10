59f876221ceb4c3db8c1d9de5bd5b8c9d7b96f99
kpam92 <kpam1992@gmail.com>
2017-04-10 16:50:49 -0700

    ar

--- active-record/lib/02_searchable.rb
+++ active-record/lib/02_searchable.rb
@@ -3,10 +3,25 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map {|x| x = "#{x} = ?"}.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+    parse_all(results)
   end
+
 end
 
+
+
+
 class SQLObject
+  extend Searchable
   # Mixin Searchable here...
 end

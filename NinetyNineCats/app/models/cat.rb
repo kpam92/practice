5de6ab71b35624b14a02baa72b3f778f548489b3
kpam92 <kpam1992@gmail.com>
2017-04-11 12:19:18 -0700

    cat model

--- NinetyNineCats/app/models/cat.rb
+++ NinetyNineCats/app/models/cat.rb
@@ -1,2 +1,16 @@
 class Cat < ActiveRecord::Base
+
+  validates :color, inclusion: CAT_COLORS
+  validates :sex, inclusion: %w(M F)
+
+  CAT_COLORS = %w(black white orange brown)
+
+  validates(
+  :birth_date,
+  :color,
+  :name,
+  :sex,
+  presence: true
+
+  
 end

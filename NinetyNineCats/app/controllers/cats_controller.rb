68dbe9c8feecfb24dbb8ee8d1e99aa0fd2d20a53
kpam92 <kpam1992@gmail.com>
2017-04-11 12:32:02 -0700

    cat model

--- NinetyNineCats/app/controllers/cats_controller.rb
+++ NinetyNineCats/app/controllers/cats_controller.rb
@@ -2,7 +2,29 @@ class CatsController < ApplicationController
 
   def index
     @cats = Cat.all
+    render :index
   end
 
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      flash.now[:errors] = @cat.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cats).permit(:age,:birth_date,:color,:description, :name, :sex)
+  end
 
 end

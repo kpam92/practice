1d09bd71c7d9a4e6b3f4b55f0996058ccc00aff9
kpam92 <kpam1992@gmail.com>
2017-04-11 12:27:36 -0700

    about to get to create cat page

--- NinetyNineCats/app/models/cat.rb
+++ NinetyNineCats/app/models/cat.rb
@@ -1,11 +1,11 @@
 class Cat < ActiveRecord::Base
 
   include ActionView::Helpers::DateHelper
+  CAT_COLORS = %w(black white orange brown)
 
   validates :color, inclusion: CAT_COLORS
   validates :sex, inclusion: %w(M F)
 
-  CAT_COLORS = %w(black white orange brown)
 
   validates(
   :birth_date,

af70060bbca8046243d0b42a7dc9c89e2caf54ab
kpam92 <kpam1992@gmail.com>
2017-04-11 12:17:10 -0700

    before migration, just created db

--- NinetyNineCats/db/migrate/20170411190658_create_cats.rb
+++ NinetyNineCats/db/migrate/20170411190658_create_cats.rb
@@ -1,9 +1,12 @@
 class CreateCats < ActiveRecord::Migration
   def change
     create_table :cats do |t|
+      t.date :birth_date, null: false
+      t.string :color, null: false
       t.string :name, null: false
-      t.date :birth_date
-      t.integer :
+      t.string :sex, null: false
+      t.text :description, null: false
+      
       t.timestamps null: false
     end
   end

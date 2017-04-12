316ceb8dafddf464409cf6d802a1e037d2303f6f
kpam92 <kpam1992@gmail.com>
2017-04-11 18:21:04 -0700

    rental model

--- NinetyNineCats/app/models/cat_rental_request.rb
+++ NinetyNineCats/app/models/cat_rental_request.rb
@@ -8,5 +8,19 @@ class CatRentalRequest < ActiveRecord::Base
   primary_key: :id,
   foreign_key: :cat_id,
   class_name: 'Cat'
+
+  def overlapping_requests
+    CatRentalRequest
+    .where.not(id: self.id)
+    .where(cat_id: cat_id)
+    .where(<<-SQL, start_date: start_date, end_date: end_date)
+       NOT( (start_date > :end_date) OR (end_date < :start_date) )
+    SQL
+  end
+
+  def overlapping_approved_requests
+  end
+
   
+
 end

43b1c2d0c1b3945ef92b9dfe67672ddfddf0a643
kpam92 <kpam1992@gmail.com>
2017-04-11 17:15:01 -0700

    after migration

--- NinetyNineCats/db/schema.rb
+++ NinetyNineCats/db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411190658) do
+ActiveRecord::Schema.define(version: 20170411235410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cat_rental_requests", force: :cascade do |t|
+    t.integer  "cat_id",                         null: false
+    t.date     "start_date",                     null: false
+    t.date     "end_date",                       null: false
+    t.string   "status",     default: "Pending", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
 
   create_table "cats", force: :cascade do |t|
     t.date     "birth_date",  null: false

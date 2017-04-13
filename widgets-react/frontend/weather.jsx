0a81736bc72e84636124f52ca9ce1651f0c7353e
kpam92 <kpam1992@gmail.com>
2017-04-12 17:55:09 -0700

    weather component

--- widgets-react/frontend/weather.jsx
+++ widgets-react/frontend/weather.jsx
@@ -1,17 +1,77 @@
+
 import React from 'react';
 
+const toQueryString = (obj) => {
+  let parts = [];
+  for (let i in obj) {
+      if (obj.hasOwnProperty(i)) {
+          parts.push(`${encodeURIComponent(i)}=${encodeURIComponent(obj[i])}`);
+      }
+  }
+  return parts.join('&');
+}
+
 class Weather extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: null
+    };
+    this.pollWeather = this.pollWeather.bind(this);
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(this.pollWeather);
+  }
+
+  pollWeather(location) {
+    let lat = location.coords.latitude;
+    let long = location.coords.longitude;
+    let url = 'http://api.openweathermap.org/data/2.5/weather?';
+    let params = {
+      lat: location.coords.latitude,
+      lon: location.coords.longitude
+    };
+    url += toQueryString(params);
+    const apiKey = '924774c63602d3c5954864e205c2e2f';
+    // This is our API key; please use your own!
+    url += `&APPID=${apiKey}`;
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = () => {
+      //ready state of DONE means this is complete
+      if (xmlhttp.status === 200 && xmlhttp.readyState === XMLHttpRequest.DONE) {
+        const data = JSON.parse(xmlhttp.responseText);
+        this.setState({weather: data});
+      }
+    };
+
+    xmlhttp.open('GET', url, true);
+    xmlhttp.send();
   }
 
   render() {
-    return(
-      <div>
-        <h1>WEATHER</h1>
-      </div>
-    )
-  }
-}
+    let content = <div></div>;
 
-export default Weather;
+    if (this.state.weather) {
+      let weather = this.state.weather;
+      let temp = (weather.main.temp - 273.15) * 1.8 + 32;
+      content = <div>
+                  <p>{weather.name}</p>
+                  <p>{temp.toFixed(1)} degrees</p>
+                </div>;
+    } else {
+      content = <div className='loading'>loading weather...</div>;
+    }
+    return (
+      <div>
+        <h1>Weather</h1>
+        <div className='weather'>
+          {content}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Weather

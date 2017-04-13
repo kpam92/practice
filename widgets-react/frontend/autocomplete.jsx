7d52766cbc7b2211376330324efbb8c5bf6a4133
kpam92 <kpam1992@gmail.com>
2017-04-12 19:15:37 -0700

    autocomplete

--- widgets-react/frontend/autocomplete.jsx
+++ widgets-react/frontend/autocomplete.jsx
@@ -3,17 +3,53 @@ import React from 'react';
 class AutoComplete extends React.Component {
   constructor(props){
     super(props)
-    this.state = { inputVal : ""}
+    this.state = { inputVal : ""};
+    this.selectName = this.selectName.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+
+  handleInput(event) {
+  this.setState({inputVal: event.currentTarget.value});
+  }
+
+  selectName(event) {
+    let name = event.currentTarget.innerText;
+    this.setState({inputVal: name});
+  }
+
+  matches() {
+    const matches = [];
+    if (this.state.inputVal.length === 0) {
+      return this.props.names;
+    }
+
+    this.props.names.forEach(name => {
+      let sub = name.slice(0, this.state.inputVal.length);
+      if (sub.toLowerCase() === this.state.inputVal.toLowerCase()) {
+        matches.push(name);
+      }
+    });
+
+    if (matches.length === 0) {
+      matches.push('No matches');
+    }
+
+    return matches;
   }
 
   render(){
+    let results = this.matches().map((result, i) => {
+      return (
+        <li key={i} onClick={this.selectName}>{result}</li>
+      );
+    });
     return(
       <div>
-      <h1>AUTOCOMPLETE</h1>
+      <h1>QUEENS</h1>
+        <input onChange={this.handleInput} value={this.state.inputVal} placeholder='Search...'/>
       <ul>
-        {
-          this.props.names.map((name,idx)=> <li key={idx}>{name}</li> )
-        }
+        {results}
       </ul>
     </div>
     )

4b802690d0155994c40a6cf4bd24c8f1a83c05e1
kpam92 <kpam1992@gmail.com>
2017-04-13 16:55:05 -0700

    initial setup

---
+++ todo-react/frontend/root.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Root extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    return(
+      <h1>this is the root talking</h1>
+    )
+  }
+}
+
+export default Root;

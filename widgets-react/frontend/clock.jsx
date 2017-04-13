bed32ce6d92d598bb48479c2fb1b549ea0f8e7c2
kpam92 <kpam1992@gmail.com>
2017-04-12 17:07:30 -0700

    display date without interval

--- widgets-react/frontend/clock.jsx
+++ widgets-react/frontend/clock.jsx
@@ -3,11 +3,22 @@ import React from 'react';
 class Clock extends React.Component {
   constructor(props){
     super(props)
+    this.state = { time: new Date() }
+    this.tick = this.tick.bind(this)
   }
+  
+  tick(event) {
+    event.preventDefault();
+  }
+
 
   render(){
     return(
+      <div>
       <h1>CLOCK</h1>
+      <span>{this.state.time.getHours()}:{this.state.time.getMinutes()}:{this.state.time.getSeconds()}</span>
+      </div>
+
     )
   }
 }

b2e1eb81cdbb038d5876589db074ab85a9a8793c
kpam92 <kpam1992@gmail.com>
2017-04-13 17:49:14 -0700

    forgot redux

--- todo-react/todo_redux.jsx
+++ todo-react/todo_redux.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './frontend/root';
+import configureStore from './frontend/store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
+	const store = configureStore();
+	window.store = store;
 	const root = document.getElementById("root");
 	ReactDOM.render(<Root/>, root);
 });

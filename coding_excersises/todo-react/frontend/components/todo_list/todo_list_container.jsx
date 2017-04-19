7838bad8c4242f24fcb60faddcfcc38b5deee9ff
kpam92 <kpam1992@gmail.com>
2017-04-18 20:31:51 -0700

    solved dispatch problem, remember to always include {} brackets when exporting constants or files with multiple things exported

--- coding_excersises/todo-react/frontend/components/todo_list/todo_list_container.jsx
+++ coding_excersises/todo-react/frontend/components/todo_list/todo_list_container.jsx
@@ -1,7 +1,7 @@
 import { connect} from 'react-redux';
 import TodoList from './todo_list'
 import { allTodos } from '../../reducers/selectors';
-import receiveTodo from '../../actions/todo_actions';
+import {receiveTodo} from '../../actions/todo_actions';
 
 const mapStateToProps = (state, ownProps) => ({
   todos: allTodos(state)

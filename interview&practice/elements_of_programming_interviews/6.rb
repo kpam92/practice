f1f6461e1e0a796530f2060a8b5ec594904b6c76
kpam92 <kpam1992@gmail.com>
2017-04-19 11:51:46 -0700

    attempt 2

--- interview&practice/elements_of_programming_interviews/6.rb
+++ interview&practice/elements_of_programming_interviews/6.rb
@@ -6,4 +6,29 @@
 # first-attempt
 
 def array_sorter(array,index)
+  pivot = array[index]
+  less,equal,greater = [],[],[]
+  until array.empty?
+    if array[0] < pivot
+      less << array.shift
+    elsif array[0] > pivot
+      greater << array.shift
+    else
+      equal << array.shift
+    end
+  end
+  less.concat(equal).concat(greater)
+end
+
+# This method has three seperate empty arrays, then iterates through
+# the array, and splits up the numbers accordingly. It iterates through
+# the array, so time complexity is O(n); numbers are moved around instead
+# of copied, so space complexity is O(1)
+
+# This can be improved in terms of space by doing everything in place.
+# So instead of sorting into three subarrays to concat later, let's just
+# work with the single array
+
+
+def array_sorter(array,index)
 end

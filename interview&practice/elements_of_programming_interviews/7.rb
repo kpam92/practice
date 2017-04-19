3e4ab439f57fb16ad6970905f859fc0d784c9734
kpam92 <kpam1992@gmail.com>
2017-04-19 15:23:17 -0700

    ch 7 prob

---
+++ interview&practice/elements_of_programming_interviews/7.rb
@@ -0,0 +1,11 @@
+
+# 7.1 Implement methods that take a string representing
+# an integer and return the corresponding integer, and
+# vice-versa. Should handle negative numbers
+# &&& NO LIBRARY FUNCTIONS (i.e. 'to_s' and 'to_i')
+
+def str_to_int(string)
+end
+
+def int_to_str(integer)
+end
---
+++ interview&practice/interview_questions/misc_questions.md
@@ -0,0 +1,10 @@
+This is a page to put miscellaneous software development
+related questions and answers to them.
+
+* What is dynamic programming?
+
+* What happens when you type in 'www.google.com' and press enter?
+
+* What are the upsides to ES6, and fat arrow functions?
+
+* 

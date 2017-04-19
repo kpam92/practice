e1fd23a5738e4edec4395c23dd5b5425be9c905a
kpam92 <kpam1992@gmail.com>
2017-04-18 20:16:57 -0700

    form

--- coding_excersises/todo-react/frontend/components/todo_list/todo_form.jsx
+++ coding_excersises/todo-react/frontend/components/todo_list/todo_form.jsx
@@ -4,18 +4,30 @@ import React from 'react';
 class TodoForm extends React.Component {
   constructor(props){
     super(props);
-    this.state = { title: "", body: "", done: false }
+    this.state = { title: "", body: "", done: false };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const todo = Object.assign({}, this.state, {id: uniqueId});
+    this.props.receiveTodo;
+    this.setState({title: "", body: ""});
+  }
+
+  update(property) {
+    return e => this.setState({[property]: e.target.value});
   }
 
   render() {
     return(
       <div>
-        <h1>this is the form everyone</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>title: </label>
           <input type="text" placeholder="ex: put on makeup"></input>
           <label>description: </label>
           <input type="text" placeholder="make myself beautiful"></input>
+          <button>submit</button>
         </form>
       </div>
     )

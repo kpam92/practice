b5a6e342409b9fcee006d4d5931faa589616b1cc
kpam92 <kpam1992@gmail.com>
2017-04-18 19:39:11 -0700

    todo form in todo index container

--- coding_excersises/todo-react/frontend/components/todo_list/todo_list.jsx
+++ coding_excersises/todo-react/frontend/components/todo_list/todo_list.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { allTodos } from '../../reducers/selectors'
 import TodoListItem from './todo_list_item'
+import TodoForm from './todo_form'
 class TodoList extends React.Component {
   constructor(props) {
     super(props)
@@ -21,7 +22,7 @@ class TodoList extends React.Component {
         <ul className="todo-list">
           { todoItems }
         </ul>
-        
+
       </div>
       )
 

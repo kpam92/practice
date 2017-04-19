455a6695cdaa9a6d29dbe990299c89009eb32d54
kpam92 <kpam1992@gmail.com>
2017-04-18 19:43:50 -0700

    form is rendering

--- coding_excersises/todo-react/frontend/components/todo_list/todo_form.jsx
+++ coding_excersises/todo-react/frontend/components/todo_list/todo_form.jsx
@@ -2,7 +2,15 @@ import { uniqueId } from '../../util/id_gen';
 import React from 'react';
 
 class TodoForm extends React.Component {
+  constructor(props){
+    super(props)
+  }
 
+  render() {
+    return(
+      <h1>this is the form everyone</h1>
+    )
+  }
 };
 
 export default TodoForm;

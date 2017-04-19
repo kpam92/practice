4fbaa1f2e870184fb0cd9e91933a2442dcf30704
kpam92 <kpam1992@gmail.com>
2017-04-18 19:50:56 -0700

    form state

--- coding_excersises/todo-react/frontend/components/todo_list/todo_form.jsx
+++ coding_excersises/todo-react/frontend/components/todo_list/todo_form.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 
 class TodoForm extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = { title: "", body: "", done: false }
   }
 
   render() {

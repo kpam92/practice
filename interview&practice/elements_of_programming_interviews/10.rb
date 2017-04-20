a205e3b94da0f05f03dd89746b4c67a7db6a6214
kpam92 <kpam1992@gmail.com>
2017-04-19 20:09:17 -0700

    prob 10

--- interview&practice/elements_of_programming_interviews/10.rb
+++ interview&practice/elements_of_programming_interviews/10.rb
@@ -17,5 +17,29 @@ end
 
 
 def balanced_tree?(root)
+
+  #basecase of if there is no child
+  return 0 if root.nil?
+  #basecase of if this node has no children
   return 1 if @right.nil? && @left.nil? && !@parent.nil?
+
+  #this is a recursive call down the tree
+  left = balanced_tree?(@left)
+  right = balanced_tree?(@right)
+
+  #this will return a false if somewhere down the line there
+  #was an unbalance in subtrees
+  return false if (left == false || right == false)
+
+  #if both left and right calls returned a number, then
+  #this code will check that it only has, at most, a difference
+  #of 1
+
+  difference = (left - right).abs
+  if difference == 1 || difference == 0
+    return [left,right].max + 1
+  else
+    return false
+  end
+
 end

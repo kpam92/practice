948ea98e80027ed4a9fc21782f9d471a2df44838
kpam92 <kpam1992@gmail.com>
2017-04-19 18:00:07 -0700

    first try of prob 9-1

--- interview&practice/elements_of_programming_interviews/9.rb
+++ interview&practice/elements_of_programming_interviews/9.rb
@@ -2,3 +2,26 @@
 # 9.1 Design a stack that has a max operation, in addition to
 # push and pop. The max method should return the maximum value
 # stored in the stack.
+
+class Stack
+
+  def initialize
+    @values = []
+  end
+
+  def push(val)
+    @values.push([val,val]) if @values.empty?
+    last_max = @values[-1][1]
+    curr_max = val > last_max ? val : last_max
+    @values.push([val,curr_max])
+  end
+
+  def pop
+    @values.pop
+  end
+
+  def max
+    return nil if @values.empty?
+    @values[-1][1]
+  end
+end

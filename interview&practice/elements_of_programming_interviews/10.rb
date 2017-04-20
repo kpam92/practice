d63c51a5bda034d036a0a2c5fcfb7b0982d00502
kpam92 <kpam1992@gmail.com>
2017-04-19 18:49:17 -0700

    BTS node class

--- interview&practice/elements_of_programming_interviews/10.rb
+++ interview&practice/elements_of_programming_interviews/10.rb
@@ -2,3 +2,14 @@
 # of a binary tree and checks whether the tree is height balanced.
 # This means that the difference in the height of its left and right
 # subtrees is at most 1.
+
+class BSTNode
+  attr_accessor :left, :right
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+end

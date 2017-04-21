798ecec62723e73742c5e667277744d7dda7796a
kpam92 <kpam1992@gmail.com>
2017-04-20 20:59:03 -0700

    create and show

--- coding_excersises/ToDo/app/controllers/api/todos_controller.rb
+++ coding_excersises/ToDo/app/controllers/api/todos_controller.rb
@@ -1,3 +1,16 @@
 class Api::TodosController < Api:ApiController
 
+  def show
+  render json: Todo.find(params[:id])
+end
+
+def create
+  @todo = Todo.new(todo_params)
+  if @todo.save
+    render json: @todo
+  else
+    render json: @todo.errors.full_messages, status: 422
+  end
+end
+
 end

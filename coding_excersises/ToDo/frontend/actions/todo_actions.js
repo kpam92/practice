3543e9ea893c0deca79f884bc68798e164f541ab
kpam92 <kpam1992@gmail.com>
2017-04-21 17:32:09 -0700

    update todo action

--- coding_excersises/ToDo/frontend/actions/todo_actions.js
+++ coding_excersises/ToDo/frontend/actions/todo_actions.js
@@ -6,6 +6,11 @@ export const RECEIVE_TODOS = "RECEIVE_TODOS";
 export const RECEIVE_TODO = "RECEIVE_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
 
+export const updateTodo = todo => dispatch =< (
+  TodoAPIUtil.updateTodo(todo)
+  .then(todo => { dispatch(receiveTodo(todo)); dispatch(clearErrors())},
+  err => dispatch(receiveErrors(err.responseJSON)))
+);
 
 export const fetchTodos = () => dispatch => (
   TodoAPIUtil.fetchTodos().then(todos => dispatch(receiveTodos(todos)))

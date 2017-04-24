d8fead938d40dded4c4f565d22f2ae68394d6eb4
kpam92 <kpam1992@gmail.com>
2017-04-23 18:08:31 -0700

    routes

--- coding_excersises/ToDo/config/routes.rb
+++ coding_excersises/ToDo/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     resources :todos, only: [:index, :show, :create, :update, :destroy] do
-      resources :steps only: [:create, :index]
+      resources :steps, only: [:create, :index]
     end
     resources :steps, only: [:update, :destroy]
   end

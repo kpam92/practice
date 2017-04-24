a68fb9caf38b371d6b08716e0c32e9b127616ce7
kpam92 <kpam1992@gmail.com>
2017-04-23 18:15:07 -0700

    steps controller

--- coding_excersises/ToDo/app/controllers/api/steps_controller.rb
+++ coding_excersises/ToDo/app/controllers/api/steps_controller.rb
@@ -1,2 +1,29 @@
 class Api::StepsController < Api::ApiController
+
+  def index
+    steps = Step.find(params[:id]).steps
+    render json: steps
+  end
+
+  def create
+    step = Step.new(step_params)
+
+    if step.save
+      render json: step
+    else
+      render json: step.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  private
+
+  def step_params
+    params.require(:step).permit(:title,:body,:done)
+  end
 end

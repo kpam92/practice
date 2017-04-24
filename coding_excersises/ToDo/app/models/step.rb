b5587d3e355eb8e59e022172b5f58e992e4d8e55
kpam92 <kpam1992@gmail.com>
2017-04-23 17:48:03 -0700

    step model

--- coding_excersises/ToDo/app/models/step.rb
+++ coding_excersises/ToDo/app/models/step.rb
@@ -1,2 +1,7 @@
 class Step < ApplicationRecord
+  validates :title, :todo, presence: true
+  validates :done, inclusion: [true, false], default: false
+  after_initialize { self.done = false if self.done.nil? }
+
+  belongs_to :todo
 end

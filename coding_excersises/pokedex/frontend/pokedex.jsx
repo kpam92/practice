d7c1e22c95db7c5e8be49ca566745e62d50213f2
kpam92 <kpam1992@gmail.com>
2017-04-24 18:17:11 -0700

    pokemon reducer

--- coding_excersises/pokedex/frontend/pokedex.jsx
+++ coding_excersises/pokedex/frontend/pokedex.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {fetchAllPokemon} from './util/api_util'
-import {receiveAllPokemon} from './actions/pokemon_actions'
+// import {fetchAllPokemon} from './util/api_util'
+// import {receiveAllPokemon} from './actions/pokemon_actions'
 // import configureStore from './store/store';
 // import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
-  window.fetchAllPokemon = fetchAllPokemon;
-  window.receiveAllPokemon = receiveAllPokemon;
+
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
---
+++ coding_excersises/pokedex/frontend/reducers/pokemon_reducer.js
@@ -0,0 +1,11 @@
+import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
+
+const pokemonReducer = (state = {}, action) => {
+  Object.freeze(state)
+  switch(action.type) {
+    case RECEIVE_ALL_POKEMON:
+      return merge({}, action.pokemon);
+    default:
+      return state;
+  }
+}

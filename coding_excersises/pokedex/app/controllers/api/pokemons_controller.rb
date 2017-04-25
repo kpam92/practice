15aa759477ca82cc87732b2086bb380cf6fb9dca
kpam92 <kpam1992@gmail.com>
2017-04-24 17:03:35 -0700

    json builders and pokemon controller

---
+++ coding_excersises/pokedex/app/controllers/api/pokemons_controller.rb
@@ -0,0 +1,17 @@
+class Api::PokemonsController < ApplicationController
+
+  def index
+    @pokemons = Pokemon.all
+  end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:image_url, :attack, :defense, :name, :poke_type, moves: [])
+  end
+
+end

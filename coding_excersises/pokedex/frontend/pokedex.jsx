4ab5104ed820a24e16652451c3b5396f5fea888c
kpam92 <kpam1992@gmail.com>
2017-04-24 17:47:06 -0700

    entry file is showing

---
+++ coding_excersises/pokedex/frontend/pokedex.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store';
+// import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+});

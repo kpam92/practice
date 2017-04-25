59259a457d3bf3f0526c8dbe119b1a67d39fe61e
kpam92 <kpam1992@gmail.com>
2017-04-24 19:59:42 -0700

    middleware thunk

--- coding_excersises/pokedex/frontend/pokedex.jsx
+++ coding_excersises/pokedex/frontend/pokedex.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {fetchAllPokemon} from './util/api_util'
-// import {receiveAllPokemon} from './actions/pokemon_actions'
-// import configureStore from './store/store';
+import {fetchAllPokemon} from './util/api_util'
+import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions'
+import configureStore from './store/store';
 // import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
-
+  const store = configureStore();
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
+  window.store = store;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
--- coding_excersises/pokedex/frontend/reducers/pokemon_reducer.js
+++ coding_excersises/pokedex/frontend/reducers/pokemon_reducer.js
@@ -5,6 +5,7 @@ const pokemonReducer = (state = {}, action) => {
   Object.freeze(state)
   switch(action.type) {
     case RECEIVE_ALL_POKEMON:
+      debugger;
       return merge({}, action.pokemon);
     default:
       return state;

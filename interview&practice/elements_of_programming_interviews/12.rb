dccd7f31bbe6fe9d45542b5983d8e5f9dc8d1973
kpam92 <kpam1992@gmail.com>
2017-04-25 17:16:18 -0700

    programming interview files

---
+++ interview&practice/elements_of_programming_interviews/12.rb
@@ -0,0 +1,8 @@
+# Searching
+
+
+# Write a method that takes a sorted array and a key and
+# returns the index of the first occurence of that key in the array.
+
+# Watch out that if you first find an occurence, that doesn't necessarily
+# mean that it is the first one.

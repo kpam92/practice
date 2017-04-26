8508da19dbb341bae5fbc176d077c104f7c706c5
kpam92 <kpam1992@gmail.com>
2017-04-25 18:16:11 -0700

    bsearch

--- interview&practice/elements_of_programming_interviews/12.rb
+++ interview&practice/elements_of_programming_interviews/12.rb
@@ -6,3 +6,19 @@
 
 # Watch out that if you first find an occurence, that doesn't necessarily
 # mean that it is the first one.
+
+# warmup doing regular bsearch with a sorted array
+
+def bsearch(array,target)
+  middle = array.length/2
+  return middle if array[middle] == target
+  return nil if array.length <=1
+
+  if array[middle] > target
+    return bsearch(array[0...middle],target)
+  else
+    left = bsearch(array[middle+1..-1],target)
+    left.nil? ? nil : left
+  end
+  
+end

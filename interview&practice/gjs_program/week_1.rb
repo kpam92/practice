63953134976b58975eb9cbd9c5fb32a82a582227
kpam92 <kpam1992@gmail.com>
2017-04-26 20:47:47 -0700

    add node class

--- interview&practice/gjs_program/week_1.rb
+++ interview&practice/gjs_program/week_1.rb
@@ -2,6 +2,18 @@
 # their Least Common Ancestor (LCA). Assume that each node has a parent pointer.
 
 # brute force method
+class BSTnode
+
+  attr_accessor :left,:right,:parent,:child
+
+  def initialize(left = nil, right = nil, parent = nil, child = nil)
+    @left = left
+    @right = right
+    @child = child
+    @parent = parent
+  end
+
+end
 
 def lca_1(node1, node2)
 end

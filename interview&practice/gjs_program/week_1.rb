4c617ffea1a51337a28f0baab3296bc93162e91d
kpam92 <kpam1992@gmail.com>
2017-04-26 20:45:01 -0700

    lca

---
+++ interview&practice/gjs_program/week_1.rb
@@ -0,0 +1,7 @@
+# Given two nodes in a binary tree, design an algorithm that computes
+# their Least Common Ancestor (LCA). Assume that each node has a parent pointer.
+
+# brute force method
+
+def lca_1(node1, node2)
+end

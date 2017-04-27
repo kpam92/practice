1b20571ac72e8057bf606388ba2334cd0a4354bc
kpam92 <kpam1992@gmail.com>
2017-04-26 21:52:15 -0700

    adding routes

--- coding_excersises/pokedex/frontend/components/root.jsx
+++ coding_excersises/pokedex/frontend/components/root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import PokemonIndexContainer from './pokemon/pokemon_index_container';
-
+import { Router, route, hashHistory } from 'react-router'
 const Root = ({ store }) => (
   <Provider store={store}>
     <PokemonIndexContainer/>

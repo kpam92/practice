a01ee7fb1d7c908002bffa845f6f1c9976f922ac
kpam92 <kpam1992@gmail.com>
2017-05-01 21:05:13 -0700

    index item

--- coding_excersises/pokedex/frontend/components/pokemon/pokemon_index_item.jsx
+++ coding_excersises/pokedex/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const PokemonIndexItem = ({pokemon, }) => (
+const PokemonIndexItem = ({ pokemon, router }) => (
   <li className="pokemon-index-item">
-
+    <Link to={`/pokemon/${pokemon.id}`}>
       <span>{pokemon.id}</span>
       <img src={pokemon.image_url} alt={pokemon.name} />
       <span>{pokemon.name}</span>
-
+    </Link>
   </li>
 );
 

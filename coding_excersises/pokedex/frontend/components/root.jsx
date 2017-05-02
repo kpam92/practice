9b47172e10b803da0758af2396ce8042b154414b
kpam92 <kpam1992@gmail.com>
2017-05-01 20:35:31 -0700

    router

--- coding_excersises/pokedex/frontend/components/root.jsx
+++ coding_excersises/pokedex/frontend/components/root.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import PokemonIndexContainer from './pokemon/pokemon_index_container';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { HashRouter as Router, Route, hashHistory } from 'react-router-dom';
 
-const Root = ({ store }) => {
-  return (
-    <Provider store={store}>
-      <Router history={hashHistory}>
-        <Route path="/" component={PokemonIndexContainer}>
-        </Route>
-      </Router>
-    </Provider>
-  );
-};
+const Root = ({ store }) => (
+  <Provider store={ store }>
+    <Router history={ hashHistory }>
+      <Route path="/" component={ PokemonIndexContainer } />
+    </Router>
+  </Provider>
+);
+
 export default Root;

97179e4edc6807d3ec3015518d6130db5678d141
kpam92 <kpam1992@gmail.com>
2017-05-02 13:06:23 -0700

    stack

--- interview&practice/IC/19.rb
+++ interview&practice/IC/19.rb
@@ -1,2 +1,19 @@
 # Implement a queue with 2 stacks. Your queue should have an enqueue
 # and a dequeue function and it should be "first in first out" (FIFO).
+
+
+class Stack
+
+  def initialize
+    @values = []
+  end
+
+  def push(x)
+    @values.push(x)
+  end
+
+  def pop
+    @values.pop(x)
+  end
+
+end

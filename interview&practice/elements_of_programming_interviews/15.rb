ec892604fee2a561846d0bb62cfb60b1f10228d9
kpam92 <kpam1992@gmail.com>
2017-05-03 11:01:47 -0700

    completed function

--- interview&practice/elements_of_programming_interviews/15.rb
+++ interview&practice/elements_of_programming_interviews/15.rb
@@ -20,6 +20,15 @@ def valid_bst?(node)
 
   left_side = valid_bst?(node.left)
   right_side = valid_bst?(node.right)
+  return [false, nil] if [false,nil] if left_side[0] == false || right_side[0] == false
 
+  answer = nil
+
+  if (left_side[1] == nil || left_side[1] <= node.value)
+      && (right_side[1] == nil || right_side[1] >= node.value)
+    answer = [true, node.value]
+  end
+
+  node.parent.nil? ? answer[0] : answer
   
 end

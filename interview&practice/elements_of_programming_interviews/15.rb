0eaa4ccc660fa97f702c762632d47f1b0d1800ba
kpam92 <kpam1992@gmail.com>
2017-05-03 10:28:39 -0700

    bst

---
+++ interview&practice/elements_of_programming_interviews/15.rb
@@ -0,0 +1,15 @@
+# Write a program that take as input a binary
+# tree and checks if the tree satisfies the
+# BST property.
+
+class BSTNode
+  attr_accessor :left, :right, :parent, :value
+  attr_reader :data
+
+  def initialize(value, parent = nil)
+    @value = value
+    @parent = parent
+    @left = nil
+    @right = nil
+  end
+end

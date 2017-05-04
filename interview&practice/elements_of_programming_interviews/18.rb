29913007b045a04ae21d752665ce23374e9a0a43
kpam92 <kpam1992@gmail.com>
2017-05-04 15:42:16 -0700

    greedy prob

---
+++ interview&practice/elements_of_programming_interviews/18.rb
@@ -0,0 +1,4 @@
+# Design an algorithm that takes as input an array and a number,
+# and determines if there are three entries in the array
+# (not necessarily distinct) which add up to the specified number.
+# This means you can use use any amount of the integers in the array.

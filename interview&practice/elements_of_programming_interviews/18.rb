dd8f83df9efeff321907fe4c40a852f84340d7f1
kpam92 <kpam1992@gmail.com>
2017-05-04 16:08:11 -0700

    three sum

--- interview&practice/elements_of_programming_interviews/18.rb
+++ interview&practice/elements_of_programming_interviews/18.rb
@@ -3,6 +3,9 @@
 # (not necessarily distinct) which add up to the specified number.
 # This means you can use use any amount of the integers in the array.
 
+
+# we're going to assume that the array is sorted,
+# and that we only want distinct ones
 def three_sum(array,target)
   
 end

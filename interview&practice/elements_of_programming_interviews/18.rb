65ac525d37336116c898692daddaaa3924ebdd3c
kpam92 <kpam1992@gmail.com>
2017-05-04 16:10:58 -0700

    working through prob

--- interview&practice/elements_of_programming_interviews/18.rb
+++ interview&practice/elements_of_programming_interviews/18.rb
@@ -7,5 +7,19 @@
 # we're going to assume that the array is sorted,
 # and that we only want distinct ones
 def three_sum(array,target)
-  
+
+  array.each_with_index do |base,idx|
+    idx1 = idx + 1
+    idx2 = array.length - 1
+
+    until idx1 == idx2
+      calculation = base + array[idx1] + array[idx2]
+      if calculation == target
+        return true
+      elsif calculation < target
+        idx1 += 1
+      else
+        idx2 -= 1
+      end
+    end
 end

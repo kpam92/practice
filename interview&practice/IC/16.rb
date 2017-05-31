de54e68dd932ffea1b0dbc7316bf6380285b68aa
kpam92 <kpam1992@gmail.com>
2017-05-30 20:14:10 -0700

    cake thief

---
+++ interview&practice/IC/16.rb
@@ -0,0 +1,8 @@
+# As a thief, you bring in a duffel bag that can carry only X weight.
+# Input is an array of cakes, where the first value is the weight of a
+# a single cake, and the second is it's worth for one cake. Create a
+# function that returns the maximum monetary value the duffel can hold.
+
+
+def cake_thief(cakes, duffel_max)
+end

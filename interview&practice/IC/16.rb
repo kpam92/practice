4f6a4a132b92e1a28e1fb8714ca22e0a2660c4ba
kpam92 <kpam1992@gmail.com>
2017-05-30 20:59:45 -0700

    first try

--- interview&practice/IC/16.rb
+++ interview&practice/IC/16.rb
@@ -5,4 +5,23 @@
 
 
 def cake_thief(cakes, duffel_max)
+
+  #pushes a ratio value of value to cake
+  cakes.each do |cake|
+      cake.unshift(cake[0].to_f/cake[1].to_f)
+  end
+
+  cakes.sort! {|x,y| x[0] <=> y[0]}
+
+  result = 0
+
+  while cakes.length > 0
+    current_cake = cakes[1]
+    return result if current_cake[1] > duffel_max
+    current_max = duffel_max/current_cake[1]
+    result += current_max * current_cake[2]
+    duffel_max -= current_max * current_cake[1]
+    cakes.shift
+  end
+  result
 end

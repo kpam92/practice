6d3b2316c933093a93f38604beef63560d40eb7d
kpam92 <kpam1992@gmail.com>
2017-06-05 16:20:35 -0700

    permutation

---
+++ interview&practice/gjs_program/week_1/permutations.rb
@@ -0,0 +1,8 @@
+# Given a length, interpet all the different permutations there are
+# of that length with the letters A and B. For example, input 2 would
+# result in ['AA','AB','BA', 'BB']
+
+
+def perm(length)
+  
+end

f5bd19553a9f941a62cb66413b25f10c0ffdda49
kpam92 <kpam1992@gmail.com>
2017-06-05 17:19:14 -0700

    perm

--- interview&practice/gjs_program/week_1/permutations.rb
+++ interview&practice/gjs_program/week_1/permutations.rb
@@ -23,10 +23,3 @@ end
 # This recursively calls on itself, appending all possible combinations
 # on to the two letters. It's time complexity would be 0(2^n), where
 # n is the length given
-
-# let's try iteratively
-
-def perm_it(length)
-  result = []
-  current = "A" * length
-end

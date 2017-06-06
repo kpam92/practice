e954335a8bad264b1f297c033a8fa402f374cb92
kpam92 <kpam1992@gmail.com>
2017-06-05 17:22:39 -0700

    perm

--- interview&practice/gjs_program/week_1/permutations.rb
+++ interview&practice/gjs_program/week_1/permutations.rb
@@ -23,3 +23,20 @@ end
 # This recursively calls on itself, appending all possible combinations
 # on to the two letters. It's time complexity would be 0(2^n), where
 # n is the length given
+
+# again but with "A,B,C"
+
+def perm(length)
+  return ['A','B', 'C'] if length < 2
+
+  result = []
+  appended_end = perm(length - 1)
+  current = ['A','B','C']
+
+  current.each do |letter|
+    appended_end.each do |latter_part|
+      result.push(letter+latter_part)
+    end
+  end
+  result
+end

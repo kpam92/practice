23f8fef7f9cafe78963975d9b017ff21888e327a
kpam92 <kpam1992@gmail.com>
2017-06-10 23:39:08 -0700

    first try

--- interview&practice/gjs_program/week_1/valid_parens.rb
+++ interview&practice/gjs_program/week_1/valid_parens.rb
@@ -1,3 +1,26 @@
 # Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 #
 # The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+
+def is_valid(s)
+    partners = { '('=> ")", "{"=> "}", "[" => "]" }
+
+    stack = []
+
+    idx = 0
+
+    while idx < s.length
+        if partners[s[idx]]
+            stack.push(s[idx])
+        else
+            if s[idx] == partners[stack[-1]]
+                stack.pop
+            else
+                false
+            end
+        end
+        idx += 1
+    end
+    stack.empty? ? true : false
+end

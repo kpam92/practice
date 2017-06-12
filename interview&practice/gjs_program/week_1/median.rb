f1b853d854bb244a30a9e3f662c821cffeaa2c2e
kpam92 <kpam1992@gmail.com>
2017-06-12 12:44:58 -0700

    finished problem, looking to optimize

--- interview&practice/gjs_program/week_1/median.rb
+++ interview&practice/gjs_program/week_1/median.rb
@@ -2,32 +2,50 @@
 #
 # Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 
-
+require 'byebug'
 def find_median_sorted_arrays(nums1, nums2)
-    total_length = nums1.length + nums2.length
-    if total_length.odd?
-        odd_median(nums1,nums2,total_length/2)
-    else
-        even_median(nums1,nums2,total_length/2)
-    end
-end
 
-def odd_median(nums1,num2,length)
+    return nil if nums1.empty? && nums2.empty?
+
+    half_length = (nums1.length + nums2.length) / 2
+
     count = 0
+    curr_before_median = nil
+    curr_median = nil
 
     idx1 = 0
     idx2 = 0
-    curr_median = nil
-    while count < length
-        if nums1[idx1] < nums2[idx2]
-            count += 1
-            return nums1[idx1] if count + 1 == length
-            idx1 += 1
-            count += 1
-        else
-            count += 1
-            return nums2[idx2] if count + 1 == length
-            idx2 += 1
-            count += 1
-        end
+
+    until count == half_length + 1
+
+      if nums2[idx2].nil?
+        curr_before_median = curr_median
+        curr_median = nums1[idx1]
+        idx1 += 1
+      elsif  nums1[idx1].nil?
+        curr_before_median = curr_median
+        curr_median = nums2[idx2]
+        idx2 += 1
+      elsif (nums1[idx1] < nums2[idx2])
+        curr_before_median = curr_median
+        curr_median = nums1[idx1]
+        idx1 += 1
+      elsif  nums1[idx1] >= nums2[idx2]
+        curr_before_median = curr_median
+        curr_median = nums2[idx2]
+        idx2 += 1
+      end
+
+      count += 1
+
     end
+
+    if (nums1.length + nums2.length).even?
+      diff = (curr_median - curr_before_median).to_f / 2
+      return curr_before_median + diff
+    else
+      return curr_median
+    end
+end
+
+puts find_median_sorted_arrays([1,2],[3,4])

499bc324725463311eff52cde3bd97f3f806b9e7
kpam92 <kpam1992@gmail.com>
2017-06-22 12:43:38 -0700

    k to last node

---
+++ interview&practice/IC/25.rb
@@ -0,0 +1,16 @@
+# You have a linked list ↴ and want to find the kkth to last node.
+# Write a function kth_to_last_node() that takes an integer kk and the head_node of a singly linked list, and returns the kkth to last node in the list.
+
+
+class LinkedListNode
+
+  attr_accessor :value, :next
+
+  def initialize(value)
+      @value = value
+      @next  = nil
+  end
+end
+
+def k_to_last(k, node)
+end

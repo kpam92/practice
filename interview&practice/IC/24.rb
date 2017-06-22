c43dee357eea8547a1220eb13eaaf47bb90af589
kpam92 <kpam1992@gmail.com>
2017-06-21 20:38:59 -0700

    reverse linked list

---
+++ interview&practice/IC/24.rb
@@ -0,0 +1,19 @@
+# Write a function for reversing a linked list ↴ . Do it in-place ↴ .
+#
+# Your function will have one input: the head of the list.
+#
+# Your function should return the new head of the list.
+
+class LinkedListNode
+
+  attr_accessor :value, :next
+
+  def initialize(value)
+      @value = value
+      @next  = nil
+  end
+end
+
+
+def reverse_list(node)
+end

0a0ae8d849c05f78c956fe4b9074641abfb71935
kpam92 <kpam1992@gmail.com>
2017-06-27 12:02:58 -0700

    merge ranges

---
+++ interview&practice/IC/4.rb
@@ -0,0 +1,6 @@
+# Your company built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available.
+# To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored as arrays ↴ of integers [start_time, end_time]. These integers represent the number of 30-minute blocks past 9:00am.
+# Write a function merge_ranges() that takes an array of meeting time ranges and returns an array of condensed ranges.
+
+def merge_ranges(ranges)
+end

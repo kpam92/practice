6c695f5c3216176e53d171d50c51fe1506d39795
kpam92 <kpam1992@gmail.com>
2017-06-27 09:58:23 -0700

    working on second IC question

---
+++ interview&practice/IC/2.rb
@@ -0,0 +1,8 @@
+# You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.
+# Write a function get_products_of_all_ints_except_at_index() that takes an array of integers and returns an array of the products.
+# Do not use division in your solution.
+
+
+
+def except_curr_prod(array)
+end

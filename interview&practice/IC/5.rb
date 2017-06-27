2f03cc5779e01508cc24db6155a1fefde81c0a06
kpam92 <kpam1992@gmail.com>
2017-06-27 12:56:49 -0700

    coin denominations

---
+++ interview&practice/IC/5.rb
@@ -0,0 +1,13 @@
+# Your quirky boss collects rare, old coins...
+# They found out you're a programmer and asked you to solve something they've been wondering for a long time.
+#
+# Write a function that, given:
+#
+# an amount of money
+# an array of coin denominations
+# computes the number of ways to make the amount of money with coins of the available denominations.
+
+
+def coin_computer(amount, array)
+  
+end

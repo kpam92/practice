863ed8c5bcd212d332d0e235e0991bb4f9e019a5
kpam92 <kpam1992@gmail.com>
2017-06-27 14:50:54 -0700

    binary search tree valid

---
+++ interview&practice/IC/9.rb
@@ -0,0 +1,28 @@
+# Write a function to check that a binary tree ↴ is a valid binary search tree
+
+
+class BinaryTreeNode
+
+  attr_accessor :value
+  attr_reader :left, :right
+
+  def initialize(value)
+      @value = value
+      @left  = nil
+      @right = nil
+  end
+
+  def insert_left(value)
+      @left = BinaryTreeNode.new(value)
+      return @left
+  end
+
+  def insert_right(value)
+      @right = BinaryTreeNode.new(value)
+      return @right
+  end
+end
+
+
+def valid_search_tree(head)
+end

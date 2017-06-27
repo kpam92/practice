05eeb138918596adf1536265332a3ae20381d44f
kpam92 <kpam1992@gmail.com>
2017-06-27 15:31:08 -0700

    valid search tree solution

--- interview&practice/IC/9.rb
+++ interview&practice/IC/9.rb
@@ -25,4 +25,27 @@ end
 
 
 def valid_search_tree(head)
+  node_valid?(head)[1]
 end
+
+def node_valid?(node)
+  return [nil,true] if node.nil?
+  return [node.value,true] if node.right.nil? && node.left.nil?
+
+  left = node_valid?(node.left)
+  right = node_valid?(node.right)
+
+  return [nil,false] if left[1] == false || right[1] == false
+
+  if (left.nil?|| left[0] < node.value) && (right.nil?|| right[0] > node.value)
+    return [node.value, true]
+  else
+    return [nil,false]
+  end
+end
+
+a  = BinaryTreeNode.new(2)
+a.insert_left(1)
+a.insert_left(3)
+
+puts valid_search_tree(a)

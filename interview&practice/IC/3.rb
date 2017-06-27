9cad3df570749f9e36978c6e671def8fa3918b2e
kpam92 <kpam1992@gmail.com>
2017-06-27 10:51:22 -0700

    highest prod setup

---
+++ interview&practice/IC/3.rb
@@ -0,0 +1,8 @@
+# Given an array of integers, find the highest product you can get from three of the integers.
+# The input array_of_ints will always have at least three integers.
+
+# things to look out for
+ # - highest product can include two negative integers
+
+ def highest_three_prod(array)
+ end

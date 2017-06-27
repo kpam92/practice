1ccd071a73c9f8699a5ba0678f4f1e332770fe91
kpam92 <kpam1992@gmail.com>
2017-06-27 12:52:37 -0700

    explanation

--- interview&practice/IC/4.rb
+++ interview&practice/IC/4.rb
@@ -37,13 +37,13 @@ def merge_ranges(ranges)
   result
 end
 
-# 
+#
 # how it works:
 # This solution first sorts the ranges by the beginning numbers. We store
 # the current range. If the current range falls within the current held variable,
 # we alter the current variable and continue. If the range is afterwards, we push
 # the current variable on to our result, and hold a new range. This takes O(n) space
-# and time, where n is the length of the array of ranges.
+# and O(nlogn)time (because we sorted), where n is the length of the array of ranges.
 
 # p merge_ranges([[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]])
 # p merge_ranges([[1, 2], [2, 3]])

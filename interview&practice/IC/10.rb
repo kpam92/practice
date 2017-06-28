afd09129af0f94abe02d4d667f3c0a0cce3b3f43
kpam92 <kpam1992@gmail.com>
2017-06-28 10:52:19 -0700

    rework for extra case

--- interview&practice/IC/10.rb
+++ interview&practice/IC/10.rb
@@ -22,9 +22,11 @@ class BinaryTreeNode
 end
 
 def second_largest(head)
-
+  left = false
   if head.right.nil?
-    return go_left(head.left)
+    return nil if head.left.nil?
+    head = head.left
+    left = true
   end
 
   parent = nil
@@ -32,37 +34,32 @@ def second_largest(head)
     parent = head
     head = head.right
   end
-  parent.value
+  left ? head.value : parent.value
 end
 
-def go_left(head)
-  until head.right.nil?
-    head = head.right
-  end
-  head.value
-end
 
 # How it works:
 # Because this is a binary search tree with all nodes on the right being
 # larger than those on the left, the second largest element will always be
 # the parent of the right most node. So, we traverse down the right of the
 # tree, and return the parent of the rightmost element. This take O(1) space,
-# and O(n) time, where n is the height of the tree.
+# and O(n) time, where n is the height of the tree. If there is no right element,
+# we traverse to the rightmost node of the head's left side.
 
 
-a = BinaryTreeNode.new(3)
-b = BinaryTreeNode.new(1)
+# a = BinaryTreeNode.new(3)
+# b = BinaryTreeNode.new(1)
 # c = BinaryTreeNode.new(5)
 # d = BinaryTreeNode.new(0)
-e = BinaryTreeNode.new(2)
+# e = BinaryTreeNode.new(2)
 # f = BinaryTreeNode.new(4)
 # g = BinaryTreeNode.new(6)
 #
-a.left = b
+# a.left = b
 # a.right = c
 # b.left = d
-b.right = e
+# b.right = e
 # c.left = f
 # c.right = g
-#
+
 puts second_largest(a)

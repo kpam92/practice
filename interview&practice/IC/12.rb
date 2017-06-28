2dfb7def581eba7614a51a5569fca51e87bfaad5
kpam92 <kpam1992@gmail.com>
2017-06-28 11:29:01 -0700

    sorted solution

--- interview&practice/IC/12.rb
+++ interview&practice/IC/12.rb
@@ -1,5 +1,14 @@
 # Suppose we had an array ↴ of nn integers sorted in ascending order. How quickly could we check if a given integer is in the array?
 
 
-def integer_checker(array)
+def integer_checker(target, array)
+  halfway = array.length/2
+  return true if array[halfway] == target
+  return false if array.length < 2
+
+  if array[halfway] > target
+    integer_checker(target,array[0..halfway])
+  else
+    integer_checker(target,array[halfway..-1])
+  end
 end

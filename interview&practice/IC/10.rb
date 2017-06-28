b22e040bfa51e66ceccd4e3ea412d6bbb9c138f1
kpam92 <kpam1992@gmail.com>
2017-06-28 10:49:02 -0700

    rework for other situation

--- interview&practice/IC/10.rb
+++ interview&practice/IC/10.rb
@@ -22,12 +22,24 @@ class BinaryTreeNode
 end
 
 def second_largest(head)
+
+  if head.right.nil?
+    return go_left(head.left)
+  end
+
   parent = nil
   until head.right.nil?
     parent = head
     head = head.right
   end
   parent.value
+end
+
+def go_left(head)
+  until head.right.nil?
+    head = head.right
+  end
+  head.value
 end
 
 # How it works:
@@ -38,19 +50,19 @@ end
 # and O(n) time, where n is the height of the tree.
 
 
-# a = BinaryTreeNode.new(3)
-# b = BinaryTreeNode.new(1)
+a = BinaryTreeNode.new(3)
+b = BinaryTreeNode.new(1)
 # c = BinaryTreeNode.new(5)
 # d = BinaryTreeNode.new(0)
-# e = BinaryTreeNode.new(2)
+e = BinaryTreeNode.new(2)
 # f = BinaryTreeNode.new(4)
 # g = BinaryTreeNode.new(6)
 #
-# a.left = b
+a.left = b
 # a.right = c
 # b.left = d
-# b.right = e
+b.right = e
 # c.left = f
 # c.right = g
 #
-# puts second_largest(a)
+puts second_largest(a)

c28a1e1cd16c2d1c50a0684558757cd2b8e5bf99
kpam92 <kpam1992@gmail.com>
2017-06-28 11:33:05 -0700

    solution explanation

--- interview&practice/IC/12.rb
+++ interview&practice/IC/12.rb
@@ -7,8 +7,19 @@ def integer_checker(target, array)
   return false if array.length < 2
 
   if array[halfway] > target
-    integer_checker(target,array[0..halfway])
+    integer_checker(target,array[0...halfway])
   else
-    integer_checker(target,array[halfway..-1])
+    integer_checker(target,array[halfway+1..-1])
   end
 end
+
+# How it works:
+Because the array is sorted, the fastest way to check through for a
+specific value is to search in binary fashion. This takes O(1) space
+and O(logn) time.
+
+# puts integer_checker(2,[1,2,3,4,5,7,9,44])
+# puts integer_checker(9,[1,2,3,4,5,7,9,44])
+# puts integer_checker(44,[1,2,3,4,5,7,9,44])
+# puts integer_checker(1,[1,2,3,4,5,7,9,44])
+# puts integer_checker(0,[1,2,3,4,5,7,9,44])

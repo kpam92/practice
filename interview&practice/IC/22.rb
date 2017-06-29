750fcc90eeffabc735b0ca36b100c94d90a3fa58
kpam92 <kpam1992@gmail.com>
2017-06-29 13:24:32 -0700

    delete node

---
+++ interview&practice/IC/22.rb
@@ -0,0 +1,14 @@
+# Delete a node from a singly-linked list ↴ , given only a variable pointing to that node.
+
+class LinkedListNode
+
+  attr_accessor :value, :next
+
+  def initialize(value)
+      @value = value
+      @next  = nil
+  end
+end
+
+def delete_node(node)
+end

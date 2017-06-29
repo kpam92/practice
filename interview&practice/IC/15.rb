351b16152c050da40d4f1333f50d33445ddab924
kpam92 <kpam1992@gmail.com>
2017-06-29 12:38:34 -0700

    no recursion solution

--- interview&practice/IC/15.rb
+++ interview&practice/IC/15.rb
@@ -10,9 +10,32 @@ end
 
 
 def fib_no_rec(number)
+  x = 0
+  y = 1
+
+  return x if number == 0
+  return y if number == 1
+
+  number -= 1
+
+  while number > 0
+    curr_num = x + y
+    x = y
+    y = curr_num
+    num -= 1
+  end
+
+  curr_num
 end
-puts fib(0)
-puts fib(1)
-puts fib(2)
-puts fib(3)
-puts fib(4)
+
+# puts fib(0)
+# puts fib(1)
+# puts fib(2)
+# puts fib(3)
+# puts fib(4)
+
+puts fib_no_rec(0)
+puts fib_no_rec(1)
+puts fib_no_rec(2)
+puts fib_no_rec(3)
+puts fib_no_rec(4)

92a3093a15b1e99929089115ffe75f0fb5e1f039
kpam92 <kpam1992@gmail.com>
2017-06-29 13:44:28 -0700

    test cases

--- interview&practice/IC/26.rb
+++ interview&practice/IC/26.rb
@@ -1,4 +1,22 @@
 # Write a function to reverse a string in place.
 
 def reverse_string(string)
+  idx1 = 0
+  idx2 = string.length - 1
+
+  while idx1 < idx2
+    string[idx1],string[idx2] = string[idx2],string[idx1]
+
+    idx1 += 1
+    idx2 -= 1
+  end
+  string
 end
+
+
+puts reverse_string('anna')
+puts reverse_string('banana')
+puts reverse_string('hello kitty')
+puts reverse_string('hello world')
+puts reverse_string('this is a sentence')
+puts reverse_string('whats up')

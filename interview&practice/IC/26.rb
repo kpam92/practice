69614a78ad43f029af98418000cd43605ce7cfa0
kpam92 <kpam1992@gmail.com>
2017-06-29 13:48:13 -0700

    solution explanation

--- interview&practice/IC/26.rb
+++ interview&practice/IC/26.rb
@@ -14,9 +14,14 @@ def reverse_string(string)
 end
 
 
-puts reverse_string('anna')
-puts reverse_string('banana')
-puts reverse_string('hello kitty')
-puts reverse_string('hello world')
-puts reverse_string('this is a sentence')
-puts reverse_string('whats up')
+# how it works:
+# This uses two indices, and we traverse through the letters from the beginning
+# and end, and we swap elements, and stop when they meet. This has O(1) space and
+# O(n) time, where n is the length of the string. (more specifically O(n/2) time)
+
+# puts reverse_string('anna')
+# puts reverse_string('banana')
+# puts reverse_string('hello kitty')
+# puts reverse_string('hello world')
+# puts reverse_string('this is a sentence')
+# puts reverse_string('whats up')

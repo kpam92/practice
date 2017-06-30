30eb29e0afede04ffafc11bf43b03bdbb2c04659
kpam92 <kpam1992@gmail.com>
2017-06-30 10:56:17 -0700

    compound word

---
+++ interview&practice/gjs_program/1.rb
@@ -0,0 +1,8 @@
+# Given a sorted list of words, find the longest compound word in the list that is constructed by concatenating the words in the list.
+
+# input :[‘cat’, ‘cats’, ‘catsdogcats’, ‘catxdogcatsrat’, ‘dog’, ‘dogcatsdog’, ‘hippopotamuses’, ‘rat’, ‘ratcat’, ‘ratcatdog’, ‘ratcatdogcat’]
+# output: 'ratcatdogcat'
+
+def longest_compound(array)
+  
+end

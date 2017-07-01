3db89cc3e50265ea6d1207e7e034434e037bff29
kpam92 <kpam1992@gmail.com>
2017-06-30 19:12:50 -0700

    solution

--- interview&practice/IC/28.rb
+++ interview&practice/IC/28.rb
@@ -6,4 +6,22 @@
 # Example: if the example string above is input with the number 10 (position of the first parenthesis), the output should be 79 (position of the last parenthesis).
 
 def par_partner(string,index)
+  queue = []
+
+  idx = index + 1
+
+  while idx < string.length
+
+    if string[idx] == "("
+      queue.push("(")
+    elsif string[idx] == ")" && queue.empty?
+      return idx
+    elsif string[idx] == ")"
+      queue.pop(1)
+    end
+
+    idx += 1
+  end
+
+  'no partner'
 end

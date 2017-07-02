bc60e42b524cbe5c3b6321df46df13a762d7290e
kpam92 <kpam1992@gmail.com>
2017-07-01 21:45:55 -0700

    solution

--- interview&practice/IC/29.rb
+++ interview&practice/IC/29.rb
@@ -13,6 +13,26 @@
 
 
 def bracket_validator(string)
+  brackets = {
+    "(" => ")",
+    "{" => "}",
+    "[" => "]"
+  }
 
-  
+  stack = []
+
+  idx = 0
+
+  while idx < string.length
+    if string[idx] == "(" || string[idx] == "[" || string[idx] == "{"
+      stack.push(string[idx])
+    elsif string[idx] == ")" || string[idx] == "]" || string[idx] == "}"
+      return false unless brackets[stack[-1]] == string[idx]
+      stack.pop
+    end
+    idx += 1
+  end
+
+  stack.empty?
+
 end

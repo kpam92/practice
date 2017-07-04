89cc59d462599165c57096ab99fa67e0d5760b83
kpam92 <kpam1992@gmail.com>
2017-07-04 11:23:33 -0700

    riffle

---
+++ interview&practice/IC/36.rb
@@ -0,0 +1,7 @@
+# To prove this, let's write a function to tell us if a full deck of cards shuffled_deck is a single riffle of two other halves half1 and half2.
+#
+# We'll represent a stack of cards as an array of integers in the range 1..521..52 (since there are 5252 distinct cards in a deck).
+
+
+def riffle?(deck, half1,half2)
+end

93300a2927f84b3b842e302c9c09ca5c18e72872
kpam92 <kpam1992@gmail.com>
2017-07-05 13:45:15 -0700

    merge lists

---
+++ interview&practice/IC/43.rb
@@ -0,0 +1,8 @@
+# In order to win the prize for most cookies sold, my friend Alice and I are going to merge our Girl Scout Cookies orders and enter as one unit.
+# Each order is represented by an "order id" (an integer).
+#
+# We have our lists of orders sorted numerically already, in arrays. Write a function to merge our arrays of orders into one sorted array.
+
+
+def merge_arrays(array1,array2)
+end

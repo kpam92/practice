ec28fb714384a361736682c015ee948903681ef6
kpam92 <kpam1992@gmail.com>
2017-07-08 18:05:38 -0700

    spiralize

---
+++ interview&practice/gjs_program/week_1/spiral.rb
@@ -0,0 +1,11 @@
+# Create a method that will traverse through a matrix(array of arrays)
+# in a spiral fashion.
+#
+# [1,2,3,4]
+# [11,12,13,5]
+# [10,9,7,6]
+#  will return 1,2,3,4,5,6,7,8,9,10,11,12,12
+
+
+def spiralize(matrix)
+end

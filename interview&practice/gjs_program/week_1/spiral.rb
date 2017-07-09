b5d0fd646045066e1df5590512f63130f8d4e0f9
kpam92 <kpam1992@gmail.com>
2017-07-08 18:41:26 -0700

    solution works

--- interview&practice/gjs_program/week_1/spiral.rb
+++ interview&practice/gjs_program/week_1/spiral.rb
@@ -4,44 +4,39 @@
 # [1,2,3,4]
 # [11,12,13,5]
 # [10,9,7,6]
-#  will return 1,2,3,4,5,6,7,8,9,10,11,12,12
+#  will return 1,2,3,4,5,6,7,8,9,10,11,12
 
 
 def spiralize(matrix)
   result = []
-  visited = {}
-  start = [0,0]
-  length = matrix[0].length - 1
-  depth = matrix.length - 1
-  row,column = start
-  while visited[[row,column]] != true
-    # right
-    while column < length
-      visited[[row,column]] = true
-      result << matrix[row][column]
-      column += 1
-    end
-    # down
-    while row < depth
-      visited[[row,column]] = true
-      result << matrix[row][column]
-      row += 1
-    end
-    # left
-    while column >= start[0]
-      visited[[row,column]] = true
-      result << matrix[row][column]
-      column -= 1
-    end
-    # up
 
-    while row > start[1]
-      visited[[row,column]] = true
-      result << matrix[row][column]
-      row -= 1
+  until matrix.empty?
+    # top
+
+    result.concat(matrix[0])
+    matrix.shift(1)
+    # right
+    break if matrix.empty?
+    matrix.each do |row|
+      result << row[-1]
+      row.pop
     end
-    start =  [row,column]
+    # bottom
+    break if matrix.empty?
+    result.concat(matrix[matrix.length - 1].reverse)
+    matrix.pop
+    # left
+    break if matrix.empty?
+    next_row = []
+    matrix.each do |row|
+      next_row.unshift(row[0])
+      row.shift(1)
+    end
+    result.concat(next_row)
   end
+
+  result
 end
 
-puts spiralize([[1,2],[3,4]])
+matrix = [[1,2,3,4],[10,11,12,5],[9,8,7,6]]
+puts spiralize(matrix)

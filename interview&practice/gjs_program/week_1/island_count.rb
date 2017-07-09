1c6142ee20c94be236849b76fbbc958b8fd75692
kpam92 <kpam1992@gmail.com>
2017-07-08 17:05:55 -0700

    first solution

--- interview&practice/gjs_program/week_1/island_count.rb
+++ interview&practice/gjs_program/week_1/island_count.rb
@@ -18,4 +18,49 @@
 
 
 def island_count(matrix)
+
+  island_count = 0
+
+  row = 0
+
+  while row < matrix.length
+    column = 0
+    while column < row.length
+      if matrix[column][row] == 1
+
+        island_count += 1
+        matrix = sink_island(row, column, matrix)
+      end
+      column += 1
+    end
+
+    row += 1
+  end
 end
+
+def sink_island(row, column, map)
+  map[row][column] = 0
+
+  # north
+  if map[row-1][column] == 1
+    map = sink_island(row-1, column, map)
+  end
+  # south
+  if map[row+1][column] == 1
+    map = sink_island(row+1, column, map)
+  end
+  # east
+  if map[row][colum+1] == 1
+    map = sink_island(row, column+1, map)
+  end
+  # west
+  if map[row][colum-1] == 1
+    map = sink_island(row, column-1, map)
+  end
+
+  map
+end
+
+
+world_1 = [[11110],[11010],[11000],[00000]]
+puts island_count(world_1)

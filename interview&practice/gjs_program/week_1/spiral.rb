11049bc34c95239bc7d85644fe8cb23ec2a53b95
kpam92 <kpam1992@gmail.com>
2017-07-08 18:49:57 -0700

    solution reconfigure

--- interview&practice/gjs_program/week_1/spiral.rb
+++ interview&practice/gjs_program/week_1/spiral.rb
@@ -7,36 +7,38 @@
 #  will return 1,2,3,4,5,6,7,8,9,10,11,12
 
 
+# def spiralize(matrix)
+#   result = []
+#
+#   until matrix.empty?
+#     # top
+#
+#     result.concat(matrix[0])
+#     matrix.shift(1)
+#     # right
+#     break if matrix.empty?
+#     matrix.each do |row|
+#       result << row.pop
+#     end
+#     # bottom
+#     break if matrix.empty?
+#     result.concat(matrix[matrix.length - 1].reverse)
+#     matrix.pop
+#     # left
+#     break if matrix.empty?
+#     next_row = []
+#     matrix.each do |row|
+#       next_row.unshift(row.shift)
+#     end
+#     result.concat(next_row)
+#   end
+#
+#   result
+# end
+#
+# matrix = [[1,2,3,4],[10,11,12,5],[9,8,7,6]]
+# matrix = [[1,2],[3,4]]
+# puts spiralize(matrix)
+
 def spiralize(matrix)
-  result = []
-
-  until matrix.empty?
-    # top
-
-    result.concat(matrix[0])
-    matrix.shift(1)
-    # right
-    break if matrix.empty?
-    matrix.each do |row|
-      result << row[-1]
-      row.pop
-    end
-    # bottom
-    break if matrix.empty?
-    result.concat(matrix[matrix.length - 1].reverse)
-    matrix.pop
-    # left
-    break if matrix.empty?
-    next_row = []
-    matrix.each do |row|
-      next_row.unshift(row[0])
-      row.shift(1)
-    end
-    result.concat(next_row)
-  end
-
-  result
 end
-
-matrix = [[1,2,3,4],[10,11,12,5],[9,8,7,6]]
-puts spiralize(matrix)

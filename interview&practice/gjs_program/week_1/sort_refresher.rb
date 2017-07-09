47ebf0839bca0c7b1544e3a67e57cf4ef6da0bc0
kpam92 <kpam1992@gmail.com>
2017-07-09 14:08:18 -0700

    merge sort

--- interview&practice/gjs_program/week_1/sort_refresher.rb
+++ interview&practice/gjs_program/week_1/sort_refresher.rb
@@ -12,6 +12,7 @@ def bubble_sort(array)
     while idx < array.length - 1
 
       if array[idx] > array[idx + 1]
+        sorted = false
         array[idx],array[idx + 1] = array[idx + 1], array[idx]
       end
 
@@ -19,8 +20,48 @@ def bubble_sort(array)
     end
 
   end
+
+  array
 end
+
+# O(1) space, O(n^2) time
+
 # merge sort
+
+def merge_sort(array)
+
+  return array if array.length < 2
+
+  middle_idx = array.length/2
+
+  array1 = array[0...middle_idx]
+  array2 = array[middle_idx..-1]
+
+  merge(merge_sort(array1),merge_sort(array2))
+
+end
+
+def merge(array1,array2)
+
+  result = []
+
+  until array1.empty? || array2.empty?
+
+    if array1[0] < array2[0]
+      result.push(array1.shift)
+    else
+      result.push(array2.shift)
+    end
+  end
+
+  result.concat(array1).concat(array2)
+
+end
+
+# puts merge_sort([1,9,2,8,3,7,4,6,5])
+
+# O(n) space, O(nlogn) time
+
 
 # quick sort
 

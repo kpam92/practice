a44b7a59f6e9ffdd1d2af5333c931fbd5a4360d2
kpam92 <kpam1992@gmail.com>
2017-07-08 18:18:15 -0700

    solution

--- interview&practice/gjs_program/week_1/spiral.rb
+++ interview&practice/gjs_program/week_1/spiral.rb
@@ -8,4 +8,40 @@
 
 
 def spiralize(matrix)
+  result = []
+  visited = {}
+  start = [0,0]
+  length = matrix[0].length - 1
+  depth = matrix.length - 1
+  row,column = start
+  while visited[[row,column]] != true
+    # right
+    while column < length
+      visited[[row,column]] = true
+      result << matrix[row][column]
+      column += 1
+    end
+    # down
+    while row < depth
+      visited[[row,column]] = true
+      result << matrix[row][column]
+      row += 1
+    end
+    # left
+    while column >= start[0]
+      visited[[row,column]] = true
+      result << matrix[row][column]
+      column -= 1
+    end
+    # up
+
+    while row > start[1]
+      visited[[row,column]] = true
+      result << matrix[row][column]
+      row -= 1
+    end
+    start =  [row,column]
+  end
 end
+
+puts spiralize([[1,2],[3,4]])

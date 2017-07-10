bbb3de213a2b08170093b91290a69d64cfb7dd3a
kpam92 <kpam1992@gmail.com>
2017-07-10 14:17:41 -0700

    solution

--- interview&practice/gjs_program/week_1/spiral2.rb
+++ interview&practice/gjs_program/week_1/spiral2.rb
@@ -22,12 +22,50 @@ def spiralize(n)
   right = n - 1
 
   direction = 'right'
-
+  num = 1
   while top <= bottom && left <= right
-      if direction == 'right'
-      elsif direction == 'down'
-      elsif direction == 'left'
-      elsif direction == 'up'
+    if direction == 'right'
+      column = left
+      while column <= right
+        result[top][column] = num
+        column += 1
+        num += 1
       end
+      top += 1
+      direction = 'down'
+
+    elsif direction == 'down'
+      row = right
+      while row <= bottom
+        result[row][right] = num
+        row += 1
+        num += 1
+      end
+      right -= 1
+      direction = 'left'
+    elsif direction == 'left'
+      column = right
+      while column >= left
+        result[bottom][column] = num
+        column -= 1
+        num += 1
+      end
+      bottom -= 1
+      direction = 'up'
+
+    elsif direction == 'up'
+      row = bottom
+      while row <= top
+        result[row][left] = num
+        row -= 1
+        num += 1
+      end
+      left -= 1
+      direction = 'right'
+    end
   end
+
+  result
 end
+
+puts spiralize(3)

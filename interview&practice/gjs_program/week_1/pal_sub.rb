871abd6ad03df1c252bc42f863bd3431e1a71d6a
kpam92 <kpam1992@gmail.com>
2017-07-13 16:08:22 -0700

    solution excludes spaces and other characters

--- interview&practice/gjs_program/week_1/pal_sub.rb
+++ interview&practice/gjs_program/week_1/pal_sub.rb
@@ -8,41 +8,50 @@ def longest_pal(string)
   idx = 0
 
   longest_pal = string[0] == string[1] ? string[0..1] : ""
+  longest_pal_idx = string[0] == string[1] ? [0,1] : []
   idx = 1
 
   while idx < string.length
     curr_pal = string[idx]
-    idx1 = idx - 1
-    idx2 = idx + 1
-    while idx1 >= 0 && idx2 < string.length
+    curr_pal, curr_pal_idx = longest_sub(string,idx - 1, idx + 1, curr_pal)
 
-      if string[idx1] == string[idx2]
-        curr_pal = string[idx1] + curr_pal + string[idx2]
-        idx1 -= 1
-        idx2 += 1
-      else
-        break
-      end
+    if curr_pal.length > longest_pal.length
+      longest_pal = curr_pal
+      longest_pal_idx = curr_pal_idx
     end
-    longest_pal = [longest_pal,curr_pal].max
 
     curr_pal = ""
-    idx1 = idx
-    idx2 = idx + 1
+    curr_pal, curr_pal_idx = longest_sub(string,idx, idx + 1, curr_pal)
 
-    while idx1 >= 0 && idx2 < string.length
-      if string[idx1] == string[idx2]
-        curr_pal = string[idx1] + curr_pal + string[idx2]
-        idx1 -= 1
-        idx2 += 1
-      else
-        break
-      end
+    if curr_pal.length > longest_pal.length
+      longest_pal = curr_pal
+      longest_pal_idx = curr_pal_idx
     end
-    longest_pal = [longest_pal,curr_pal].max
 
     idx += 1
   end
 
-  longest_pal
+  result[longest_pal[idx][0],longest_pal_idx[1]]
 end
+
+def longest_sub(string,idx1,idx2,curr_pal)
+  while idx1 >= 0 && idx2 < string.length
+    if (!idx1 > 96 && !idx1 < 123) && (!idx2 > 96 && !idx2 < 123)
+      idx1 -= 1
+      idx2 += 1
+    elsif (!idx1 > 96 && !idx1 < 123)
+      idx1 -= 1
+    elsif (!idx2 > 96 && !idx1 < 123)
+      idx2 += 1
+    elsif string[idx1] == string[idx2]
+      curr_pal = string[idx1] + curr_pal + string[idx2]
+      idx1 -= 1
+      idx2 += 1
+    else
+      break
+    end
+  end
+  [curr_pal,[idx1,idx2]]
+end
+
+puts longest_pal("abcdababakel")

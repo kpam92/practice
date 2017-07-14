0c7d51d226785ff4573b5e0c1833b77b6653af28
kpam92 <kpam1992@gmail.com>
2017-07-14 14:30:52 -0700

    solution

--- interview&practice/gjs_program/week_1/shifted_binary.rb
+++ interview&practice/gjs_program/week_1/shifted_binary.rb
@@ -6,7 +6,39 @@
 
 
 def shifted_array_search(array,target)
+  start_index = shift_array(array)
+  array = array.map.with_index {|x,idx| x = [x,idx]}
+  array = array[start_index..-1].concat(array[0...start_index])
+
+  result_index = binary_search(array, target)
+  return -1 if result_index == -1
+  array[result_index][1]
+
 end
+
+def binary_search(array,target)
+  middle_idx = array.length/2
+
+  return middle_idx if array[middle_idx][0] == target
+  return -1 if array.length < 2
+
+  if array[middle_idx][0] > target
+    return binary_search(array[0...middle_idx],target)
+  else
+    curr_result = binary_search(array[middle_idx..-1],target)
+    return -1 if curr_result == -1
+    return middle_idx + curr_result
+  end
+
+end
+# puts binary_search([1,2,3,4,5,6,7,8,9],1) # 0
+# puts binary_search([1,2,3,4,5,6,7,8,9],9) # 8
+# puts binary_search([1,2,3,4,5,6,7,8,9],7) # 6
+# puts binary_search([1,2,3,4,5,6,7,8,9],3) # 2
+# puts binary_search([1,2,3,4,5,6,7,8,9],100) # -1
+# puts binary_search([1,2,3,4,5,6,7,8,9],-1) # -1
+
+
 
 def shift_array(array)
 
@@ -27,5 +59,20 @@ def shift_array(array)
   middle_idx
 
 end
+# puts shift_array([4,5,6,7,8,9,10,11,12,13,14,15,16,1])
 
-puts shift_array([4,5,6,7,8,9,10,11,12,13,14,15,16,1])
+
+
+
+
+
+
+
+# puts shifted_array_search([9, 12, 17, 2, 4, 5],1)
+# puts shifted_array_search([9, 12, 17, 2, 4, 5],100)
+# puts shifted_array_search([9, 12, 17, 2, 4, 5],9)
+# puts shifted_array_search([9, 12, 17, 2, 4, 5],12)
+# puts shifted_array_search([9, 12, 17, 2, 4, 5],17)
+# puts shifted_array_search([9, 12, 17, 2, 4, 5],2)
+# puts shifted_array_search([9, 12, 17, 2, 4, 5],4)
+# puts shifted_array_search([9, 12, 17, 2, 4, 5],5)

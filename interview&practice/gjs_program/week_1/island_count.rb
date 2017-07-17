64323c75a5dc20e9bc4ddaa360efa6ce441eae74
kpam92 <kpam1992@gmail.com>
2017-07-17 16:48:14 -0700

    iterative solution fix

--- interview&practice/gjs_program/week_1/island_count.rb
+++ interview&practice/gjs_program/week_1/island_count.rb
@@ -43,7 +43,7 @@ end
 
 def sink_island(row, column, map)
   map[row][column] = "0"
-  p map
+
   # north
   if row > 0 && map[row-1][column] == "1"
     map = sink_island(row-1, column, map)
@@ -72,7 +72,7 @@ end
 world_1 = ["11000","11000","00100","00011"]
 world_2 = ["10111","10101","11101"]
 # puts island_count(world_1)
-puts island_count(world_2)
+# puts island_count(world_2)
 # puts island_count(world_1)
 
 def island_count_iterative(matrix)
@@ -86,37 +86,45 @@ def island_count_iterative(matrix)
     while column < matrix[row].length
       if matrix[row][column] == "1"
         count += 1
-        sink_iterative(matrix, row, column)
+        map = sink_iterative(matrix, row, column)
       end
+      column += 1
     end
     row += 1
   end
-
+  count
 end
 
 def sink_iterative(map,row,column)
   queue = [[row,column]]
-  while queue.length > 1
+  while queue.length > 0
     curr_row, curr_column = queue.shift
+    # puts "row:#{curr_row} col: #{curr_column}"
     map[curr_row][curr_column] = '0'
     # north
     if curr_row > 0 && map[curr_row - 1][curr_column] == '1'
       queue << [curr_row - 1,curr_column]
     end
     # south
-    if curr_row < matrix.length - 1 && map[curr_row + 1][curr_column] == '1'
-      queue << [curr_row + 1][curr_column]
+    if curr_row < map.length - 1 && map[curr_row + 1][curr_column] == '1'
+      queue << [curr_row + 1,curr_column]
     end
     # east
-    if curr_column < matrix[curr_row] - 1 && map[curr_row][curr_column + 1] == '1'
-      queue << [curr_row][curr_column + 1]
+    if curr_column < map[curr_row].length - 1 && map[curr_row][curr_column + 1] == '1'
+      queue << [curr_row,curr_column + 1]
     end
     # west
     if curr_column > 0 && map[curr_row][curr_column - 1] == '1'
-      queue << [curr_row][curr_column - 1]
+      queue << [curr_row,curr_column - 1]
     end
   end
+
+  map
 end
 
-puts island_count(world_1)
-puts island_count(world_2)
+
+
+
+# puts island_count_iterative(world_1)
+
+# puts island_count_iterative(world_2)

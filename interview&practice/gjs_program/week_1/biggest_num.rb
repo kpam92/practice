80a5d062f31cb7a6f806a38e10abe071eab74299
kpam92 <kpam1992@gmail.com>
2017-07-19 10:49:21 -0700

    biggest num

---
+++ interview&practice/gjs_program/week_1/biggest_num.rb
@@ -0,0 +1,7 @@
+# Given two arrays of integers, build the maximum number possible from all the input numbers without violating the internal order of either of the original arrays.
+#
+# Example: [1,3,2], [9,1,1] => 913211
+# [1,1,2,1], [1,1,1] => 1121111
+
+def biggest_num(array1,array2)
+end

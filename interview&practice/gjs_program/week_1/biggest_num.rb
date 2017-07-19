8180bdb8cfadd30aabe9357df6e58ed4d4c4cb07
kpam92 <kpam1992@gmail.com>
2017-07-19 11:11:26 -0700

    solution

--- interview&practice/gjs_program/week_1/biggest_num.rb
+++ interview&practice/gjs_program/week_1/biggest_num.rb
@@ -4,6 +4,26 @@
 # [1,1,2,1], [1,1,1] => 1121111
 
 def biggest_num(array1,array2)
+  result = ""
+
+  until array1.empty? || array2.empty?
+    if array1[0] > array2[0]
+      result += array1.shift.to_s
+    elsif array2[0] > array1[0]
+      result += array2.shift.to_s
+    elsif array1[0] == array2[0]
+
+      array, num = next_biggest(array1,array2)
+      if array == 1
+        result += array1.shift(num).map(&:to_s).join
+      else
+        result += array2.shift(num).map(&:to_s).join
+      end
+    end
+
+  end
+  # result += ((array1.map(&:to_s).join) + (array2.map(&:to_s).join))
+  result.to_i
 end
 
 def next_biggest(array1,array2)
@@ -12,10 +32,10 @@ def next_biggest(array1,array2)
 
   while idx < [array1.length,array2.length].min
     if array1[idx] == array2[idx] && (array2.length - 1) == idx && array1.length > array2.length
-      return [1,idx]
+      return [1,idx + 1]
     elsif array1[idx] == array2[idx] && (array1.length - 1) == idx && array2.length > array1.length
-      return [2,idx]
-    elsif array1[idx1] > array2[idx]
+      return [2,idx + 1]
+    elsif array1[idx] > array2[idx]
       return [1,idx]
     elsif array2[idx] > array1[idx]
       return [2,idx]
@@ -23,7 +43,9 @@ def next_biggest(array1,array2)
       idx += 1
     end
   end
-  return [1, idx - 1]
+  return [1, idx]
 end
 
-puts next_biggest([1,1,2,1],[1,1,1])
+# puts biggest_num([1,3,2], [9,1,1])
+puts next_biggest([1,1,2], [1,1,1])
+puts biggest_num([1,1,2,1], [1,3,1])

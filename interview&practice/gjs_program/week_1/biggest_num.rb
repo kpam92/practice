37f428822aac63a2d708af208fbf31fce36f1726
kpam92 <kpam1992@gmail.com>
2017-07-19 10:57:08 -0700

    helper function

--- interview&practice/gjs_program/week_1/biggest_num.rb
+++ interview&practice/gjs_program/week_1/biggest_num.rb
@@ -5,3 +5,25 @@
 
 def biggest_num(array1,array2)
 end
+
+def next_biggest(array1,array2)
+
+  idx = 0
+
+  while idx < [array1.length,array2.length].min
+    if array1[idx] == array2[idx] && (array2.length - 1) == idx && array1.length > array2.length
+      return [1,idx]
+    elsif array1[idx] == array2[idx] && (array1.length - 1) == idx && array2.length > array1.length
+      return [2,idx]
+    elsif array1[idx1] > array2[idx]
+      return [1,idx]
+    elsif array2[idx] > array1[idx]
+      return [2,idx]
+    elsif array1[idx] == array2[idx]
+      idx += 1
+    end
+  end
+  return [1, idx - 1]
+end
+
+puts next_biggest([1,1,2,1],[1,1,1])

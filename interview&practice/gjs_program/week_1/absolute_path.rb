d11964754deff105f16ebc00cf964a4e277ba460
kpam92 <kpam1992@gmail.com>
2017-07-25 13:21:13 -0700

    absolute path

---
+++ interview&practice/gjs_program/week_1/absolute_path.rb
@@ -0,0 +1,10 @@
+# Given an absolute path for a file (Unix-style), simplify it.
+#
+# For example,
+# path = "/home/", => "/home"
+# path = "/a/./b/../../c/", => "/c"
+# click to show corner cases.
+
+
+def absolute_path(string)
+end

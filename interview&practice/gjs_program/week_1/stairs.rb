5748f0e5f9bc41fb4f95f6135f3a078b990cad27
kpam92 <kpam1992@gmail.com>
2017-07-24 21:44:23 -0700

    solution explanation

--- interview&practice/gjs_program/week_1/stairs.rb
+++ interview&practice/gjs_program/week_1/stairs.rb
@@ -17,17 +17,27 @@ end
 # fails when I get to the input of 38. So let's try a bottom-up approach.
 
 
-def climb_stairs(n)
+def climb_stairs_bottom_up(n)
 
-  t = Array.new(n, 0)
+  t = Array.new(n + 1, 0)
 
   climb_helper(n,t)
 end
 
 def climb_helper(n,t)
-  
+  return t[n] if t[n] > 0
+
+  if n == 0 || n == 1
+    t[n] = 1
+  else
+    t[n] = climb_helper(n - 1,t) + climb_helper(n - 2, t)
+  end
+  t[n]
 end
 
-# puts climb_stairs(38)
+# This bottom-up solution keeps an array of all the values' output so that
+# it doesn't have to keep recalculating the same functions over and over again
+
+# puts climb_stairs_bottom_up(38)
 # puts climb_stairs(4)
 # puts climb_stairs(5)

5af905f6fcc50babde0995b8cc305e5477dce724
kpam92 <kpam1992@gmail.com>
2017-07-24 20:58:06 -0700

    stairs solution

--- interview&practice/gjs_program/week_1/stairs.rb
+++ interview&practice/gjs_program/week_1/stairs.rb
@@ -1,9 +1,13 @@
-You are climbing a stair case. It takes n steps to reach to the top.
-
-Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
-Note: Given n will be a positive integer.
+# You are climbing a stair case. It takes n steps to reach to the top.
+#
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+#
+# Note: Given n will be a positive integer.
 
 
 def climb_stairs(n)
+  return 1 if n == 1
+  return 3 if n == 2
+
+  climb_stairs(n - 1) + climb_stairs(n - 2)
 end

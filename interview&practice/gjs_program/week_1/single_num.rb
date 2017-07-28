cbc8a090ba59bdc5af2e404dcc39fba141e7e265
kpam92 <kpam1992@gmail.com>
2017-07-27 18:44:26 -0700

    single num

--- interview&practice/gjs_program/week_1/single_num.rb
+++ interview&practice/gjs_program/week_1/single_num.rb
@@ -3,5 +3,20 @@
 # Note:
 # Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
+
+# this solution would normally be solved using an XOR operator, but we are going
+# to use a hash that will operate on O(n) space
+
 def find_single_num(array)
+  nums = Hash.new
+
+  array.each do |num|
+    if nums[num]
+      nums.delete[num]
+    else
+      nums[num] = true
+    end
+  end
+
+  nums.keys[0]
 end

811c119c4c06c7dc4e1c231fbfbd3f10f40122b0
kpam92 <kpam1992@gmail.com>
2017-08-07 21:16:29 -0700

    flatten solution

--- interview&practice/gjs_program/week_1/flatten.rb
+++ interview&practice/gjs_program/week_1/flatten.rb
@@ -1,4 +1,19 @@
 # Write a function that accepts a multi dimensional array and returns a flattened version.
 
 def flatten_array(array)
+  idx = 0
+  result = []
+  
+  until array.empty?
+
+    if array[idx].is_a?(Array)
+      result.concat(flatten_array(array.shift))
+    else
+      result.push(array.shift)
+    end
+
+    idx += 1
+  end
+
+  result
 end

1c90d0ad7f6763824544ce1ae298d588e7ebf2bb
kpam92 <kpam1992@gmail.com>
2017-08-07 21:25:34 -0700

    solution explanationsn and slight fix

--- interview&practice/gjs_program/week_1/flatten.rb
+++ interview&practice/gjs_program/week_1/flatten.rb
@@ -1,19 +1,24 @@
 # Write a function that accepts a multi dimensional array and returns a flattened version.
 
 def flatten_array(array)
-  idx = 0
+
   result = []
-  
+
   until array.empty?
 
-    if array[idx].is_a?(Array)
+    if array[0].is_a?(Array)
       result.concat(flatten_array(array.shift))
     else
       result.push(array.shift)
     end
-
-    idx += 1
   end
 
   result
 end
+
+# How it works:
+# This solution iterates through the array, shifting off the first element,
+# adding it to the result if its a number, and performing flatten_array if its
+# another array. This takes O(n) space and time, where n is the number of elements.
+
+# print flatten_array([1,[2,[3,4],5],6])

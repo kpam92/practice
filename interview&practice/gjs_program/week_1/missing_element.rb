a4a9b7ae606eacb1343b06758ddd3fe9858c7c90
kpam92 <kpam1992@gmail.com>
2017-08-09 19:01:23 -0700

    solution and test case

--- interview&practice/gjs_program/week_1/missing_element.rb
+++ interview&practice/gjs_program/week_1/missing_element.rb
@@ -3,4 +3,8 @@
 # There is an array of non-negative integers. A second array is formed by shuffling the elements of the first array and deleting a random element. Given these two arrays, find which element is missing in the second array.
 
 def missing_element(array1, array2)
+  array1.reduce(:+) - array2.reduce(:+)
 end
+
+
+puts missing_element([1,2,3,4,5],[5,2,4,3])

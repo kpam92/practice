cdf4ee51e3f614c15cc82e4fba7ee00c4e3320c8
kpam92 <kpam1992@gmail.com>
2017-08-09 19:00:12 -0700

    missing element

---
+++ interview&practice/gjs_program/week_1/missing_element.rb
@@ -0,0 +1,6 @@
+# Find Missing Element
+#
+# There is an array of non-negative integers. A second array is formed by shuffling the elements of the first array and deleting a random element. Given these two arrays, find which element is missing in the second array.
+
+def missing_element(array1, array2)
+end

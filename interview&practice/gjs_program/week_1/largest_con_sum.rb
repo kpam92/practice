9b2e06f67cdd4f21f4a0d7d6f867d919f4e4e5c5
kpam92 <kpam1992@gmail.com>
2017-08-09 19:14:19 -0700

    solution

--- interview&practice/gjs_program/week_1/largest_con_sum.rb
+++ interview&practice/gjs_program/week_1/largest_con_sum.rb
@@ -1,4 +1,22 @@
  # Given an array of integers (positive and negative) find the largest continuous sum.
 
  def largest_con_sum(array)
+
+   largest_sum = array[0]
+   curr_max = largest_sum
+
+   idx = 0
+
+   while idx < array.length
+     curr_max = [curr_max + array[idx], array[idx]].max
+     largest_sum = [curr_max, largest_sum].max
+     idx += 1
+   end
+
+   largest_sum
  end
+
+
+puts largest_con_sum([1,2,-100,400,1])
+# puts largest_con_sum([])
+# puts largest_con_sum([])

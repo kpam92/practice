c30d46eddeba2e644df53789b31935c99ff70940
kpam92 <kpam1992@gmail.com>
2017-08-17 15:47:22 -0700

    problems

--- interview&practice/wb/1.rb
+++ interview&practice/wb/1.rb
@@ -32,7 +32,7 @@ def caesar_cipher(string, shift)
   idx = 0
 
   while idx < string.length
-    
+
     if string[idx].ord > 96 && string[idx].ord < 123
       curr_ord = string[idx].ord + shift
 
@@ -49,4 +49,19 @@ def caesar_cipher(string, shift)
   end
 
   string
+end
+
+# Write a function, longest_common_substring(str1, str2) that takes two strings and returns the longest common substring. A substring is defined as any consecutive slice of letters from another string.
+
+def longest_common_substring(string1, string2)
+end
+
+
+# Write a function that takes an array of integers and returns their sum. Use recursion.
+
+def array_sum(array)
+  curr_num = array.pop
+  return curr_num if array.empty?
+
+  curr_num + array_sum(array)
 end

4ae3b6ec25ddb0c3b3646a4d6326387f7d7f25f7
kpam92 <kpam1992@gmail.com>
2017-08-17 15:29:34 -0700

    longest common substring

---
+++ interview&practice/wb/2.rb
@@ -0,0 +1,4 @@
+# Write a function, longest_common_substring(str1, str2) that takes two strings and returns the longest common substring. A substring is defined as any consecutive slice of letters from another string.
+
+def longest_common_substring(string1, string2)
+end

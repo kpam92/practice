246346fc23eb5f4c0c0f1d17475d2958ab8acd26
kpam92 <kpam1992@gmail.com>
2017-08-17 16:25:31 -0700

    silly years

---
+++ interview&practice/wb/4.rb
@@ -0,0 +1,7 @@
+# Write a function that takes a year (four digit integer) and returns an array with the 10 closest subsequent years that meet the following condition: the first two digits summed with the last two digits are equal to the middle two digits. E.g.,
+#
+# 1978 => 19 + 78 = 97
+# 2307 => 23 + 07 = 30
+
+def silly_years(number)
+end

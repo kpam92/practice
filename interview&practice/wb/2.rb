5aa34f24baf749942d061cecd415effca3fb338d
kpam92 <kpam1992@gmail.com>
2017-08-17 16:00:30 -0700

    fibs

---
+++ interview&practice/wb/2.rb
@@ -0,0 +1,9 @@
+# Write a function, fibs(num) which returns the first n elements from the fibonnacci sequence, given n.
+#
+# Solve it both iteratively and recursively.
+
+def fibs_it(n)
+end
+
+def fibs_rec(n)
+end

f8b20e097527ac42db5d6ec26c51bf150d9f90b0
kpam92 <kpam1992@gmail.com>
2017-08-17 16:19:24 -0700

    is pal

--- interview&practice/wb/2.rb
+++ interview&practice/wb/2.rb
@@ -25,3 +25,41 @@ def fibs_rec(n)
 
   last_fib.push(last_fib[-2] + last_fib[-1])
 end
+
+# Write a JavaScript function that takes a string and returns true if it's a palindrome, false if it's not. Use JavaScript.
+#
+# This solution takes less time and memory than rebuilding the string backward and comparing the two.
+
+def is_pal?(string)
+
+  idx1 = 0
+  idx2 = string.length - 1
+
+  while idx1 < idx2
+    return false if string[idx1] != string[idx2]
+
+    idx1 += 1
+    idx2 -= 1
+  end
+
+  true
+end
+
+
+# JavaScript solution
+
+# function isPal(string) {
+#   let idx1 = 0
+#   let idx2 = 0
+#
+#   while (idx1 < idx2) {
+#     if (string[idx1] != string[idx2]) {
+#       return false
+#     };
+#
+#     idx1 ++
+#     idx2 ++
+#   }
+#
+#   return true
+# }

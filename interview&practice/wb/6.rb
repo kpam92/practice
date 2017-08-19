f410c1951c5847e6817197f1ae115a118c5aee9c
kpam92 <kpam1992@gmail.com>
2017-08-18 20:10:54 -0700

    subsetter

--- interview&practice/wb/6.rb
+++ interview&practice/wb/6.rb
@@ -1,4 +1,11 @@
 # Write a function that takes an array and returns all of its subsets. How many sets will it return?
 
 def subsetter(array)
+  return [[]] if array.empty?
+
+  curr_val = array[0]
+  subs = subsetter(array.drop(1))
+  new_subs = subs.map {|sub| sub + [curr_val]}
+
+  subs + new_subs
 end

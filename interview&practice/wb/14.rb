9a339dbb39e8007fc6f3c524dd548be11f31a5a2
kpam92 <kpam1992@gmail.com>
2017-08-20 18:29:32 -0700

    print spiral

---
+++ interview&practice/wb/14.rb
@@ -0,0 +1,4 @@
+# Given a square matrix in the form of a 2D array-of-arrays, return an array consisting of the values of the array in "spiral order" (top row, then right hand side, then bottom in reverse, then up, the back again...).
+
+def print_spiral(matrix)
+end

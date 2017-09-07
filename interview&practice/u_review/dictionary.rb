dec30e9c44dc3198ad4d93ee77470ad72217667f
kpam92 <kpam1992@gmail.com>
2017-09-06 21:21:26 -0700

    dictionary sequence

--- interview&practice/u_review/dictionary.rb
+++ interview&practice/u_review/dictionary.rb
@@ -18,22 +18,67 @@ end
 
 class Trie
 
+  attr_accessor :root
   def initialize
-    @root = "*"
-    @children = {}
+    @root = Node.new('*')
   end
 
   def add_word(word)
-    curr_children = @children
+
+    curr_node = @root
+
     word.chars.each do |letter|
-      curr_node = curr_children[letter] ? curr_children[letter] : Node.new(letter)
-      curr_children = curr_node.children
+
+      child_node = curr_node.children[letter]
+      if child_node.nil?
+        child_node = Node.new(letter)
+        curr_node.add_child(child_node)
+      end
+
+      curr_node = child_node
     end
-    # signifies end of word
-    curr_children['*'] = nil
+    curr_node.add_child(Node.new('*'))
     word
   end
 
-  def word_check(word)
+  def check_word(word)
+    curr_node = @root
+    word.chars.each do |letter|
+      child_node = curr_node.children[letter]
+      return false if curr_node.nil?
+      curr_node = child_node
+    end
+    return true if curr_node.children['*']
+    false
+  end
+end
 
+
+def sequence_segmenter(string,dictionary)
+  return true if string.empty?
+  trie_dictionary = Trie.new
+  dictionary.each { |word| trie_dictionary.add_word(word) }
+  string.delete!(' ')
+
+  result = sift_helper(string,trie_dictionary)
+
+end
+
+def sift_helper(string, trie)
+  idx = 0
+  curr_node = trie.root.children[string[idx]]
+
+  while idx < string.length
+    return false if curr_node.nil?
+
+    if curr_node.children['*'] && idx == string.length - 1
+      return true
+    elsif curr_node.children['*'] && trie.root.children[string[idx + 1]]
+      curr_solution = sift_helper(string[idx+1..-1],trie)
+      return true if curr_solution
+    end
+    idx += 1
+    curr_node = curr_node.children[string[idx]]
+  end
+  false
 end

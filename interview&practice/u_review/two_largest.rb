0831e0965ca33bbe553d3f5c64d455b921d93990
kpam92 <kpam1992@gmail.com>
2017-09-12 16:29:52 -0700

    solution

--- interview&practice/u_review/two_largest.rb
+++ interview&practice/u_review/two_largest.rb
@@ -11,4 +11,34 @@
 # highest_two([]) ==> []
 
 def highest_two(array)
+
+  return [] if array.empty?
+  return [nil,array[0]] if array.length < 2
+  return array if array.length == 2
+
+  largest = array[0] > array[1] ? array[0] : array[1]
+  second_largest = array[0] < array[1] ? array[0] : array[1]
+
+  idx = 2
+
+  while idx < array.length
+
+    if array[idx] > largest
+      second_largest = largest
+      largest = array[idx]
+    elsif array[idx] > second_largest
+      second_largest = array[idx]
+    end
+
+    idx += 1
+  end
+
+  [second_largest,largest]
 end
+
+p highest_two([4,4,4,4]) == [4,4]
+p highest_two([4,4,4,4,2]) == [4,4]
+p highest_two([1,2,4]) == [2,4]
+p highest_two([4,1]) == [1,4]
+p highest_two([1]) == [nil,1]
+p highest_two([]) == []

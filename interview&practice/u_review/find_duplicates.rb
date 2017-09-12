3400a1dde2aff8b3c903174b445d7d4c992ad62d
kpam92 <kpam1992@gmail.com>
2017-09-12 16:35:03 -0700

    duplicates solution

--- interview&practice/u_review/find_duplicates.rb
+++ interview&practice/u_review/find_duplicates.rb
@@ -5,4 +5,18 @@
 # [1,2,3,4,5] ==> []
 
 def duplicates(array)
+  seen_hash = Hash.new { |h, k| h[k] = 0 }
+  result = []
+
+  array.each do |num|
+    seen_hash[num] += 1
+    result.push(num) if seen_hash[num] == 2
+  end
+
+  result
 end
+
+p duplicates([1,2,3,2,4]) == [2]
+p duplicates([1,2,3,2,1]) == [1,2]
+p duplicates([1,1,1,1,1]) == [1]
+p duplicates([1,2,3,4,5]) == []

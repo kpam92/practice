baccad4259d1a0777b490f0c58ec2926beb46cc2
kpam92 <kpam1992@gmail.com>
2017-09-12 16:54:28 -0700

    caesar cipher

--- interview&practice/gjs_program/aami/1.rb
+++ interview&practice/gjs_program/aami/1.rb
@@ -1,4 +1,34 @@
 # Write a method, digital_root(num). It should sum the digits of a positive integer. If it is greater than or equal to 10, sum the digits of the resulting number. Keep repeating until there is only one digit in the result, called the "digital root". Do not use string conversion within your method.
 
 def digital_root(num)
+  while num >= 10
+    num = digital_root_step(num)
+  end
+
+  num
+end
+
+def digital_root_step(num)
+  divisor = 1
+
+  until num % divisor == num
+    divisor *= 10
+  end
+
+  divisor /= 10
+
+  result = 0
+  while num > 0
+    remainder = num % divisor
+    result += (num - remainder) / divisor
+    num = remainder
+    divisor /= 10
+  end
+  result
+end
+
+
+# Write a function that takes a message and an increment amount and outputs the same letters shifted by that amount in the alphabet. Assume lowercase and no punctuation. Preserve spaces.
+
+def caesar_cipher(string,shift)
 end

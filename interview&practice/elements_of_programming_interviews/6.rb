295f0c9c455ca367af5fce30f837764ce8048234
kpam92 <kpam1992@gmail.com>
2017-09-14 22:15:33 -0700

    fixing small details

--- interview&practice/elements_of_programming_interviews/6.rb
+++ interview&practice/elements_of_programming_interviews/6.rb
@@ -34,14 +34,14 @@ def array_sorter(array,index)
   low_idx = 0
   equal = 0
   high_idx = array.length - 1
-
+  pivot_element = array[index]
   while equal <= high_idx
-    if array[equal] > array[index]
+    if array[equal] > pivot_element
       array[equal],array[high_idx] = array[high_idx],array[equal]
       high_idx -= 1
-    elsif array[equal] == array[index]
+    elsif array[equal] == pivot_element
       equal += 1
-    else
+    else # array[equal] < pivot_element
       array[equal], array[low_idx] = array[low_idx],array[equal]
       equal += 1
       low_idx += 1

7c55c27a1982555a0c1c52bbb06d39f48e890ca7
kpam92 <kpam1992@gmail.com>
2017-09-14 21:59:04 -0700

    sorter method

--- interview&practice/u_review/array_sorter.rb
+++ interview&practice/u_review/array_sorter.rb
@@ -3,25 +3,19 @@
 #  A[i] (the 'pivot') appear first, followed by elements equal
 #  to the pivot, followed by elements greater than the pivot.
 
-
-
-# [1,6,2,8,3,7,4,5,9]
-#    L
-#              H
-#    E
-#          I
 def array_sorter(array,index)
   low_idx = 0
   equal = 0
   high_idx = array.length - 1
+  pivot_element = array[index]
 
   while equal <= high_idx
-    if array[equal] > array[index]
-      array[equal],array[high_idx] = array[high_idx],array[equal]
+    if array[equal] > pivot_element
+      array[equal], array[high_idx] = array[high_idx],array[equal]
       high_idx -= 1
-    elsif array[equal] == array[index]
+    elsif array[equal] == pivot_element
       equal += 1
-    else
+    else # array[equal] < pivot_element
       array[equal], array[low_idx] = array[low_idx],array[equal]
       equal += 1
       low_idx += 1

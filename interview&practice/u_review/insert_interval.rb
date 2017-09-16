48860900decb3db4552c03bb8fcdd0d5aa3b1644
kpam92 <kpam1992@gmail.com>
2017-09-15 17:57:58 -0700

    insert interval solution

--- interview&practice/u_review/insert_interval.rb
+++ interview&practice/u_review/insert_interval.rb
@@ -10,7 +10,35 @@ Example 2:
 Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
 
 This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+[1,2],[3,5],[6,7],[8,10],[12,16]
+        XX   XX    XX
+                  [3,10]
 =end
 
 def insert_interval(sets,n)
+  result = []
+
+  idx = 0
+
+  until sets.empty?
+    curr_compare = sets.shift
+    if intersect?(n,curr_compare)
+      n = [[n[0],curr_compare[0]].min,[n[1],curr_compare[1]].max]
+    elsif n[1] < curr_compare[1]
+      result.push(n)
+      result.concat(sets)
+      break
+    else
+      result.push(curr_compare)
+      result.push(n) if sets.length == 0
+    end
+  end
+
+  result
 end
+
+def intersect?(array1,array2)
+  array1[1] >= array2[0] && array1[0] <= array2[1]
+end
+
+p insert_interval([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,9])

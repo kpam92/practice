3af91f6a77273bf53a632d39e361def5e3565f55
kpam92 <kpam1992@gmail.com>
2017-09-18 12:28:18 -0700

    solution

--- interview&practice/u_review/longest_parens.rb
+++ interview&practice/u_review/longest_parens.rb
@@ -7,4 +7,29 @@ Another example is ")()())", where the longest valid parentheses substring is "(
 =end
 
 def longest_parens(string)
+  stack = 0
+  longest = 0
+  curr_length = 0
+  idx = 0
+
+  while idx < string.length
+    if string[idx] == '('
+      stack += 1
+    else # string[idx] == ')'
+      if stack > 0
+        stack -= 1
+        curr_length += 2
+      else #stack == 0
+        longest = curr_length if curr_length > longest
+        curr_length = 0
+      end
+      longest = curr_length if curr_length > longest
+    end
+    idx += 1
+  end
+
+  longest
 end
+
+p longest_parens(")()())")
+p longest_parens("(()")

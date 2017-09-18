ab928c5f512085b29624053603f2c8e430d5fe6d
kpam92 <kpam1992@gmail.com>
2017-09-18 10:53:02 -0700

    working on heapify

--- interview&practice/u_review/heap/lib/heap.rb
+++ interview&practice/u_review/heap/lib/heap.rb
@@ -28,11 +28,68 @@ class BinaryMinHeap
   end
 
   def self.parent_index(child_index)
+    raise 'root has no parent' if child_index == 0
+    parent = (child_index - 1)/2
+
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    prc||= Proc.new {|x,y| y <=> x }
+
+    curr_idx = parent_idx
+    sorted = false
+    until sorted
+          children = []
+          child_1 = (curr_idx*2) + 1 < len ? (curr_idx*2) + 1 : nil
+          child_2 = (curr_idx*2) + 2 < len ? (curr_idx*2) + 2 : nil
+
+          highest_child_idx = nil
+
+          unless child_1.nil?
+            if prc.call(array[child_1], array[curr_idx]) == 1
+              highest_child_idx = child_1
+            end
+          end
+
+          unless child_2.nil?
+            if highest_child_idx && prc.call(array[child_2],array[highest_child_idx]) == 1
+              highest_child_idx = child_2
+            elsif highest_child_idx.nil? && prc.call(array[child_2],array[curr_idx]) == -1
+              highest_child_idx = child_2
+            end
+          end
+
+          if highest_child_idx.nil?
+            sorted = true
+          else
+            array[curr_idx],array[highest_child_idx] = array[highest_child_idx],array[curr_idx]
+            curr_idx  = highest_child_idx
+          end
+
+        end
+        array
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    prc||= Proc.new {|x,y| x <=> y}
+
+    curr_idx = child_idx
+    sorted = false
+    until sorted
+      if curr_idx == 0
+        sorted = true
+        break
+      end
+
+      parent_idx = (curr_idx - 1)/2
+
+      if prc.call(array[parent_idx],array[curr_idx]) == -1
+        sorted = true
+      else # parent < curr
+        array[parent_idx],array[curr_idx] = array[curr_idx],array[parent_idx]
+        curr_idx = parent_idx
+      end
+    end
+    array
   end
 end

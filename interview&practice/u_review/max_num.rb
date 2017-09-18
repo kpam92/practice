920bea99e30c72434e6111e72f37c3e1793d22ea
kpam92 <kpam1992@gmail.com>
2017-09-18 13:46:14 -0700

    max num

---
+++ interview&practice/u_review/max_num.rb
@@ -0,0 +1,10 @@
+# Given two arrays of integers, build the maximum number possible from all the input numbers without violating the internal order of either of the original arrays.
+#
+# Example: [1,3,2], [9,1,1] => 913211 [1,1,2,1], [1,1,1] => 1121111
+
+def max_num(array1,array2)
+  result = ''
+
+  until array1.empty? || array2.empty?
+  end
+end

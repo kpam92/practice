a3b1ac135aab1f059331cb2c0a87d217b408678e
kpam92 <kpam1992@gmail.com>
2017-09-18 15:38:56 -0700

    rotation point

---
+++ interview&practice/u_review/IC/rotation_point.rb
@@ -0,0 +1,21 @@
+=begin
+Write a function for finding the index of the "rotation point," which is where I started working from the beginning of the dictionary. This array is huge (there are lots of words I don't know) so we want to be efficient here.
+=end
+
+words = [
+  'ptolemaic',
+  'retrograde',
+  'supplant',
+  'undulate',
+  'xenoepist',
+  'asymptote', # <-- rotates here!
+  'babka',
+  'banoffee',
+  'engender',
+  'karpatka',
+  'othellolagkage',
+]
+
+
+def rotation_point(array)
+end

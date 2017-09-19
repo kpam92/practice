1194d2f4aaea7151cfc6b615f44c7b6dcdda4119
kpam92 <kpam1992@gmail.com>
2017-09-19 14:55:58 -0700

    routes

---
+++ interview&practice/u_review/misc/diff_routes.rb
@@ -0,0 +1,12 @@
+=begin
+Consider that the driver with one trip want to pick up some peoples in different locations like this:
+String[] locations ={
+"person1, person2, person3, person4, person5",
+" person6, person7, person8, person9",
+"person10, person11, person12",
+"person13, person14, person15",}
+in each location there are different choice, so write a code present all possible way to pick up people in the different locations. you can use every data structure needs.
+=end
+
+def routes(array)
+end

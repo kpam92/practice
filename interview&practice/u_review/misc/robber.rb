8ace6b2dfca75a0255c63adf9a8e614994260dc4
kpam92 <kpam1992@gmail.com>
2017-09-19 12:57:59 -0700

    robber solution

--- interview&practice/u_review/misc/robber.rb
+++ interview&practice/u_review/misc/robber.rb
@@ -4,4 +4,23 @@
 # Maximize the total value of valuables which he can rob.
 
 def rob(array)
+  return 0 if array.empty?
+  return array[0] if array.length == 1
+  return array.max if array.length == 2
+  result = Array.new(array.length + 1) {0}
+  result[1] = array[0]
+  result[2] = array[1]
+  idx = 0
+
+  while idx < result.length
+    result[idx] = [result[idx - 2],result[idx - 3]].max + nums[idx - 1]
+    idx += 1
+  end
+  result[-2..-1].max
 end
+=begin
+[2,0,4,3,10]
+
+ 2   6 5 16
+
+=end

ab2610e15045c58968795d6ee07da77815bbd14d
kpam92 <kpam1992@gmail.com>
2017-09-19 12:30:22 -0700

    robber prob

---
+++ interview&practice/u_review/misc/robber.rb
@@ -0,0 +1,7 @@
+# There are N number of houses in a straight line. House at index i has valuables worth V[i].
+# A thief is planning to rob as many houses as possible on this straight line.
+# Constraint : If he robs house at index i, then he cannot rob houses at indices (i - 1) and (i + 1).
+# Maximize the total value of valuables which he can rob.
+
+def rob(array)
+end

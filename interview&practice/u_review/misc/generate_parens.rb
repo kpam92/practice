2518ad0d10125d2413c2dae077ffa8da180ff5bf
kpam92 <kpam1992@gmail.com>
2017-09-20 11:52:34 -0700

    parens generator

--- interview&practice/u_review/misc/generate_parens.rb
+++ interview&practice/u_review/misc/generate_parens.rb
@@ -3,4 +3,9 @@ Generate all possible matched parenthesis, given n left parenthesis and right pa
 =end
 
 def combination_parens(n)
+  recursive_par(n - 1,n)
+end
+
+def recursive_par(open_needed,close_needed)
+
 end
---
+++ interview&practice/u_review/misc/n_parens.rb
@@ -0,0 +1,17 @@
+=begin
+Write a function to generate all possible n pairs of balanced parentheses.
+
+For example,
+
+Input : n=1
+Output: {}
+
+Input : n=2
+Output:
+{}{}
+{{}}
+=end
+
+
+def parens_print(n)
+end

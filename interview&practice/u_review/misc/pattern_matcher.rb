598c9bb7620d9d616bd5fa10b2478344c8c5886d
kpam92 <kpam1992@gmail.com>
2017-09-21 15:13:11 -0700

    solution

--- interview&practice/u_review/misc/pattern_matcher.rb
+++ interview&practice/u_review/misc/pattern_matcher.rb
@@ -8,4 +8,60 @@
 
 
 def pattern_matcher(pattern,string)
+
+  #normalize pattern to make 'a' be first
+  if pattern[0] == 'b'
+     (0...pattern.length).each do |x|
+      if pattern[x] == 'b'
+        pattern[x] = 'a'
+      else
+        pattern[x] = 'b'
+      end
+    end
+  end
+
+  pattern_helper(pattern,string,nil,nil)
 end
+
+def pattern_helper(pattern,string,a,b)
+  if pattern.length == 1
+    if pattern == 'a'
+      return true if a.nil? || string == a
+      return false
+    elsif pattern == 'b'
+      return true if b.nil? || string == b
+      return false
+    end
+  end
+  if pattern[0] == 'a' && !a.nil?
+
+    return false if string[0...a.length] != a
+    return pattern_helper(pattern[1..-1],string[a.length..-1],a,b)
+
+  elsif pattern[0] == 'b' && !b.nil?
+
+    return false if string[0...b.length] != b
+    return pattern_helper(pattern[1..-1],string[b.length..-1],a,b)
+
+  elsif pattern[0] == 'a' && a.nil?
+
+    idx = 0
+    while idx < string.length
+      curr_check = pattern_helper(pattern[1..-1],string[idx+1..-1],string[0..idx],b)
+      return curr_check if curr_check
+      idx += 1
+    end
+
+  elsif pattern[0] == 'b' && b.nil?
+
+    idx = 0
+    while idx < string.length
+      curr_check = pattern_helper(pattern[1..-1],string[idx+1..-1],a,string[0..idx])
+      return curr_check if curr_check
+      idx += 1
+    end
+
+  end
+end
+
+# puts pattern_matcher('baba',nil)

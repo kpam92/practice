fdf0f6db1889a12d7631424bf88abf4d0bdbf0ce
kpam92 <kpam1992@gmail.com>
2017-09-21 22:27:59 -0700

    iterative solution

--- interview&practice/u_review/cc/1.rb
+++ interview&practice/u_review/cc/1.rb
@@ -38,31 +38,61 @@ end
 
 # puts pal_perm?('Tact Coa')
 
-def one_edit_away(string1,string2,edits = 0)
-  # puts "#{string1} #{string2}"
-  return true if string1 == string2
-  return true if string1.empty? && string2.length == 1 && edits == 0
-  return true if string2.empty? && string1.length == 1 && edits == 0
-  return false if string1.empty? || string2.empty?
+# def one_edit_away(string1,string2,edits = 0)
+#   # puts "#{string1} #{string2}"
+#   return true if string1 == string2
+#   return true if string1.empty? && string2.length == 1 && edits == 0
+#   return true if string2.empty? && string1.length == 1 && edits == 0
+#   return false if string1.empty? || string2.empty?
+#
+#   idx1 = 0
+#   idx2 = 0
+#   while idx1 < string1.length && idx2 < string2.length
+#     if string1[idx1] == string2[idx2]
+#       idx1 += 1
+#       idx2 += 1
+#     elsif string1[idx1] != string2[idx2]
+#       return false if edits == 1
+#       option1 = one_edit_away(string1[idx1..-1],string2[idx2+1..-1],1)
+#       option2 = one_edit_away(string1[idx1+1..-1],string2[idx2..-1],1)
+#       option3 = one_edit_away(string1[idx1+1..-1],string2[idx2+1..-1],1)
+#       return option1 || option2 || option3
+#     end
+#   end
+#
+#   (string1.length - string2.length).abs > 1 ? false : true
+# end
 
-  idx1 = 0
-  idx2 = 0
-  while idx1 < string1.length && idx2 < string2.length
-    if string1[idx1] == string2[idx2]
-      idx1 += 1
-      idx2 += 1
-    elsif string1[idx1] != string2[idx2]
-      return false if edits == 1
-      option1 = one_edit_away(string1[idx1..-1],string2[idx2+1..-1],1)
-      option2 = one_edit_away(string1[idx1+1..-1],string2[idx2..-1],1)
-      option3 = one_edit_away(string1[idx1+1..-1],string2[idx2+1..-1],1)
-      return option1 || option2 || option3
+def one_edit_away(string1,string2)
+  return true if string1 == string2
+
+  if string1.length == string2.length
+    change = false
+    idx = 0
+    while idx < string1.length
+      if string1[idx] != string2[idx]
+        return false if change
+        change = true
+      end
+      idx += 1
     end
+    return true
+  else # string1.length != string2.length
+    idx = 0
+
+    while idx < string1.length && idx < string2.length
+      if string1[idx] != string2[idx]
+        option1 = string1[idx..-1] == string2[idx+1..-1]
+        option2 = string1[idx+1..-1] == string2[idx..-1]
+        return option1 || option2
+      end
+      idx += 1
+    end
+    string1.length - string2.length > 1 ? false : true
   end
 
-  (string1.length - string2.length).abs > 1 ? false : true
 end
-
+# 
 # puts one_edit_away('pale','pale')
 # puts one_edit_away('pale','bale')
 # puts one_edit_away('ale','bale')

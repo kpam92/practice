8fd360c11d2853bb2c27609e6ab7cc586721e1e3
kpam92 <kpam1992@gmail.com>
2017-09-21 20:55:34 -0700

    pal perm

--- interview&practice/u_review/cc/1.rb
+++ interview&practice/u_review/cc/1.rb
@@ -9,3 +9,29 @@ def is_perm?(string1,string2)
   return false if seen.values.reduce(:+) > 0
   true
 end
+
+# puts is_perm('kevin','nivek')
+
+def pal_perm?(string)
+  seen = Hash.new {|h,k| h[k] = 0}
+
+  string.chars.each do |x|
+    low_letter = x.downcase
+    if low_letter.ord > 96 && low_letter.ord < 123
+      seen[x.downcase] += 1
+    end
+  end
+
+  odd = false
+
+  seen.each do |key,value|
+    if value % 2 == 1
+      return false if odd
+      odd = true
+    end
+  end
+
+  true
+end
+
+# puts pal_perm?('Tact Coa')

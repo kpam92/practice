ca923d15ef8ceb5fde37dec1215ed5c29088c2c6
kpam92 <kpam1992@gmail.com>
2017-09-23 10:55:42 -0700

    triple step prob

--- interview&practice/u_review/cc/8.rb
+++ interview&practice/u_review/cc/8.rb
@@ -11,3 +11,30 @@ def fib_helper(n,memo)
   end
   memo[n]
 end
+
+def triple_step(n)
+  steps = Array.new(n+1) {0}
+
+  steps.each_with_index do |step,idx|
+    next if idx == 0
+    steps[idx] += 1 if idx == 1
+    steps[idx] += 1 if idx == 2
+    steps[idx] += 1 if idx == 3
+
+    if steps[(idx - 1)] && idx - 1 >= 0
+      steps[idx] += steps[idx - 1]
+    end
+    if steps[(idx - 2)] && idx - 2 >= 0
+      p idx
+      steps[idx] += steps[idx - 2]
+    end
+    if steps[(idx - 3)] && idx - 3 >= 0
+      steps[idx] += steps[idx - 3]
+    end
+    p steps
+  end
+  # p steps
+  steps[-1]
+end
+
+puts triple_step(5)

e29a86f7d642944a56457436eaf22ad69d31fb47
kpam92 <kpam1992@gmail.com>
2017-09-29 09:02:58 -0700

    solution n**2

--- interview&practice/u_review/misc/intersection.rb
+++ interview&practice/u_review/misc/intersection.rb
@@ -1,4 +1,22 @@
 # Given arr1 and arr2, find the intersection of both sets. It should be trivial to write an O(n**2) solution. Use sorting to solve in O(nlog(n)). Next, improve this to O(n) time (maybe use a non-array datastructure).
 
+#O(n^2)
 def intersect1(arr1,arr2)
+
+  idx1 = 0
+  while idx1 < arr1.length
+    idx2 = 0
+    while idx2 < arr2.length
+      if arr1[idx1] == arr2[idx2]
+        curr = idx1
+        while arr1[idx1] == arr2[idx2]
+          idx1 += 1
+          idx2 += 1
+        end
+        return arr1[curr...idx1]
+      end
+      idx2 += 1
+    end
+    idx1 += 1
+  end
 end

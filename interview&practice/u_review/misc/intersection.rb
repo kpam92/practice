69c9b111ed2756cb4d7407a546a1037def8b0051
kpam92 <kpam1992@gmail.com>
2017-09-29 09:07:28 -0700

    intersection nlogn

--- interview&practice/u_review/misc/intersection.rb
+++ interview&practice/u_review/misc/intersection.rb
@@ -1,22 +1,32 @@
 # Given arr1 and arr2, find the intersection of both sets. It should be trivial to write an O(n**2) solution. Use sorting to solve in O(nlog(n)). Next, improve this to O(n) time (maybe use a non-array datastructure).
 
 #O(n^2)
-def intersect1(arr1,arr2)
+def intersection1(arr1, arr2)
+  arr1.uniq.select { |el| arr2.include?(el) }
+end
+
+#O(nlogn)
+
+def interection2(arr1,arr2)
+  arr1.sort!
+  arr2.sort!
 
   idx1 = 0
-  while idx1 < arr1.length
-    idx2 = 0
-    while idx2 < arr2.length
-      if arr1[idx1] == arr2[idx2]
-        curr = idx1
-        while arr1[idx1] == arr2[idx2]
-          idx1 += 1
-          idx2 += 1
-        end
-        return arr1[curr...idx1]
-      end
+  idx2 = 0
+
+  while idx1 < arr1.length && idx2 < arr2.length
+    result = []
+
+    if arr1[idx1] == arr2[idx2]
+      result.push(arr1[idx1])
+      idx1 += 1
+      idx2 += 1
+    elsif arr1[idx1] < arr2[idx2]
+      idx1 += 1
+    else #arr1[idx1] > arr2[idx2]
       idx2 += 1
     end
-    idx1 += 1
   end
+
+  result
 end
